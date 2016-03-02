daaf1d5b5836962dbcd0471c30a33303288d4b7b
Craig Willis <willis8@willis8-9f6b.ncsa.illinois.edu>
2016-03-02 15:39:50 -0600

    Moved templates to root directory

--- apiserver/test/load-templates-clowder.sh
+++ apiserver/test/load-templates-clowder.sh
@@ -3,14 +3,14 @@ TOKEN=`curl -s -d "{\"username\": \"admin\", \"password\": \"12345\"}" -H "Conte
 HEADERS=("--header" "Authorization: Bearer ${TOKEN}" "--header" "Content-Type: application/json")
 
 TEMPLATE_PATH="templates/clowder/json/"
-curl "${HEADERS[@]}" -X PUT --data @$TEMPLATE_PATH/clowder-svc.json http://localhost:8083/services/templates/clowder/service
-curl "${HEADERS[@]}" -X PUT --data @$TEMPLATE_PATH/mongo-svc.json http://localhost:8083/services/templates/mongo/service
-curl "${HEADERS[@]}" -X PUT --data @$TEMPLATE_PATH/rabbitmq-service.json http://localhost:8083/services/templates/rabbitmq/service
-curl "${HEADERS[@]}" -X PUT --data @$TEMPLATE_PATH/elasticsearch-service.json http://localhost:8083/services/templates/elasticsearch/service
+curl "${HEADERS[@]}" -X PUT --data @$TEMPLATE_PATH/clowder-svc.json http://localhost:8083/templates/clowder/service
+curl "${HEADERS[@]}" -X PUT --data @$TEMPLATE_PATH/mongo-svc.json http://localhost:8083/templates/mongo/service
+curl "${HEADERS[@]}" -X PUT --data @$TEMPLATE_PATH/rabbitmq-service.json http://localhost:8083/templates/rabbitmq/service
+curl "${HEADERS[@]}" -X PUT --data @$TEMPLATE_PATH/elasticsearch-service.json http://localhost:8083/templates/elasticsearch/service
 
-curl "${HEADERS[@]}" -X PUT --data @$TEMPLATE_PATH/clowder-controller.json http://localhost:8083/services/templates/clowder/controller
-curl "${HEADERS[@]}" -X PUT --data @$TEMPLATE_PATH/mongo-controller.json http://localhost:8083/services/templates/mongo/controller
-curl "${HEADERS[@]}" -X PUT --data @$TEMPLATE_PATH/rabbitmq-controller.json http://localhost:8083/services/templates/rabbitmq/controller
-curl "${HEADERS[@]}" -X PUT --data @$TEMPLATE_PATH/elasticsearch-controller.json http://localhost:8083/services/templates/elasticsearch/controller
-curl "${HEADERS[@]}" -X PUT --data @$TEMPLATE_PATH/image-preview-controller.json http://localhost:8083/services/templates/image-preview/controller
+curl "${HEADERS[@]}" -X PUT --data @$TEMPLATE_PATH/clowder-controller.json http://localhost:8083/templates/clowder/controller
+curl "${HEADERS[@]}" -X PUT --data @$TEMPLATE_PATH/mongo-controller.json http://localhost:8083/templates/mongo/controller
+curl "${HEADERS[@]}" -X PUT --data @$TEMPLATE_PATH/rabbitmq-controller.json http://localhost:8083/templates/rabbitmq/controller
+curl "${HEADERS[@]}" -X PUT --data @$TEMPLATE_PATH/elasticsearch-controller.json http://localhost:8083/templates/elasticsearch/controller
+curl "${HEADERS[@]}" -X PUT --data @$TEMPLATE_PATH/image-preview-controller.json http://localhost:8083/templates/image-preview/controller
 
--- apiserver/test/load-templates-dataverse.sh
+++ apiserver/test/load-templates-dataverse.sh
@@ -3,15 +3,15 @@ TOKEN=`curl -s -d "{\"username\": \"admin\", \"password\": \"12345\"}" -H "Conte
 HEADERS=("--header" "Authorization: Bearer ${TOKEN}" "--header" "Content-Type: application/json")
 
 TEMPLATE_PATH="templates/dataverse/json/"
-curl "${HEADERS[@]}" -X PUT --data @$TEMPLATE_PATH/dataverse-svc.json http://localhost:8083/services/templates/dataverse/service
-curl "${HEADERS[@]}" -X PUT --data @$TEMPLATE_PATH/postgres-svc.json http://localhost:8083/services/templates/postgres/service
-curl "${HEADERS[@]}" -X PUT --data @$TEMPLATE_PATH/solr-svc.json http://localhost:8083/services/templates/solr/service
-curl "${HEADERS[@]}" -X PUT --data @$TEMPLATE_PATH/rserve-svc.json http://localhost:8083/services/templates/rserve/service
-curl "${HEADERS[@]}" -X PUT --data @$TEMPLATE_PATH/tworavens-svc.json http://localhost:8083/services/templates/tworavens/service
+curl "${HEADERS[@]}" -X PUT --data @$TEMPLATE_PATH/dataverse-svc.json http://localhost:8083/templates/dataverse/service
+curl "${HEADERS[@]}" -X PUT --data @$TEMPLATE_PATH/postgres-svc.json http://localhost:8083/templates/postgres/service
+curl "${HEADERS[@]}" -X PUT --data @$TEMPLATE_PATH/solr-svc.json http://localhost:8083/templates/solr/service
+curl "${HEADERS[@]}" -X PUT --data @$TEMPLATE_PATH/rserve-svc.json http://localhost:8083/templates/rserve/service
+curl "${HEADERS[@]}" -X PUT --data @$TEMPLATE_PATH/tworavens-svc.json http://localhost:8083/templates/tworavens/service
 
-curl "${HEADERS[@]}" -X PUT --data @$TEMPLATE_PATH/dataverse-rc.json http://localhost:8083/services/templates/dataverse/controller
-curl "${HEADERS[@]}" -X PUT --data @$TEMPLATE_PATH/postgres-rc.json http://localhost:8083/services/templates/postgres/controller
-curl "${HEADERS[@]}" -X PUT --data @$TEMPLATE_PATH/solr-rc.json http://localhost:8083/services/templates/solr/controller
-curl "${HEADERS[@]}" -X PUT --data @$TEMPLATE_PATH/rserve-rc.json http://localhost:8083/services/templates/rserve/controller
-curl "${HEADERS[@]}" -X PUT --data @$TEMPLATE_PATH/tworavens-rc.json http://localhost:8083/services/templates/tworavens/controller
+curl "${HEADERS[@]}" -X PUT --data @$TEMPLATE_PATH/dataverse-rc.json http://localhost:8083/templates/dataverse/controller
+curl "${HEADERS[@]}" -X PUT --data @$TEMPLATE_PATH/postgres-rc.json http://localhost:8083/templates/postgres/controller
+curl "${HEADERS[@]}" -X PUT --data @$TEMPLATE_PATH/solr-rc.json http://localhost:8083/templates/solr/controller
+curl "${HEADERS[@]}" -X PUT --data @$TEMPLATE_PATH/rserve-rc.json http://localhost:8083/templates/rserve/controller
+curl "${HEADERS[@]}" -X PUT --data @$TEMPLATE_PATH/tworavens-rc.json http://localhost:8083/templates/tworavens/controller
 
