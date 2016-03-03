82e7dd9fed1835e5f9cf11b047527d651457ec78
Craig Willis <willis8@willis8-9f6b.ncsa.illinois.edu>
2016-03-03 12:45:21 -0600

    Preliminary openstack integration for volumes. Moved to gcfg for configuration

--- apiserver/openstack/openstack.go
+++ apiserver/openstack/openstack.go
@@ -1,22 +1,50 @@
-package main
+package openstack
 
 import (
 	"bytes"
 	"encoding/json"
-	"flag"
+	//"flag"
 	"fmt"
-	"strings"
 	"io/ioutil"
 	"net/http"
+	//sys "os"
 	"os/exec"
+	"strings"
+	"time"
 )
+
+type Token struct {
+	Id      string
+	Expires time.Time
+}
+
+type Volume struct {
+	Id       string
+	Name     string
+	Device   string
+	ServerId string
+	Size     int
+}
+
+type Attachment struct {
+	id       string
+	device   string
+	serverId string
+	volumeId string
+}
 
 type OpenStack struct {
 	IdentityEndpoint string
 	VolumesEndpoint  string
 	ComputeEndpoint  string
+	Username         string
+	Password         string
+	TenantId         string
 }
 
+// OpenStack client for NDS Labs
+
+/*
 func main() {
 	flag.Parse()
 	os := OpenStack{}
@@ -27,72 +55,75 @@ func main() {
 	password := ""
 	tenantId := ""
 
-	token, _ := os.Authenticate(username, password, tenantId)
-	/*
-		vols, _ := os.ListVolumes(tenantId, token)
-		for id, name := range vols {
-			fmt.Printf("%s %s\n", id, name)
-		}
-	*/
-
-	instanceId, err := os.getInstanceId()
-	if err == nil {
-		fmt.Printf("Host instanceId: %s\n", instanceId)
-
-		//volumeId, _ := os.CreateVolume(token, tenantId, "nds-test-vol", 10)
-
-		attachments, _ := os.getVolumeAttachments(token, tenantId, instanceId)
-		for _, attachment := range attachments {
-			fmt.Printf("%s %s\n", attachment.id, attachment.device)
-		}	
-
-		volumeId := "d8888f48-8520-47ed-a3e8-8b6a3d639dfa"
-		///os.GetVolume(token, tenantId, volumeId)
-		// /etc/machine-id
-		//mountPoint := "/dev/vdf"
-		//os.createVolumeAttachment(token, tenantId, instanceId, volumeId, mountPoint)
-		//time.Sleep(time.Second*10)
-		//os.GetVolume(token, tenantId, volumeId)
-		//os.Mkfs(mountPoint)
-		//os.DetachVolume(token, tenantId, volumeId, instanceId)
-		//os.GetVolume(token, tenantId, volumeId)
-		//attachmentId := "52ba6092-205a-443b-b36c-904b0e689b33"
-		os.deleteVolumeAttachment(token, tenantId, instanceId, volumeId)
+	token, err := os.Authenticate(username, password, tenantId)
+	if err != nil {
+		fmt.Println(err)
+		sys.Exit(-1)
 	}
 
-}
+	vols, _ := os.ListVolumes(tenantId, token.Id)
+	for _, vol := range vols {
+		fmt.Printf("%s %s\n", vol.Id, vol.Name)
+	}
 
-func (s *OpenStack) getInstanceId() (string, error) {
+	//instanceId, _ := os.GetInstanceId()
+	instanceId := "e8bbb11f-fb9b-4edb-a718-9baf495af825"
+	fmt.Printf("Host instanceId: %s\n", instanceId)
+
+	//volumeId, _ := os.CreateVolume(token, tenantId, "nds-test-vol", 10)
+
+	attachments, _ := os.getVolumeAttachments(token.Id, tenantId, instanceId)
+	for _, attachment := range attachments {
+		fmt.Printf("%s %s\n", attachment.id, attachment.device)
+	}
+
+	//volumeId := "7935509a-9393-495e-b10b-35ba363c3809"
+	//os.GetVolume(token.Id, tenantId, volumeId)
+	// /etc/machine-id
+	//mountPoint := "/dev/vdf"
+	//os.AttachVolume(token, tenantId, instanceId, volumeId, mountPoint)
+	//time.Sleep(time.Second*10)
+	//os.GetVolume(token, tenantId, volumeId)
+	//os.Mkfs(mountPoint)
+	//os.DetachVolume(token, tenantId, volumeId, instanceId)
+	//os.GetVolume(token, tenantId, volumeId)
+	//attachmentId := "52ba6092-205a-443b-b36c-904b0e689b33"
+	//os.DetachVolume(token, tenantId, instanceId, volumeId)
+
+}
+*/
+
+// Returns the local host instanceId
+func (s *OpenStack) GetInstanceId() (string, error) {
 	data, err := ioutil.ReadFile("/etc/machine-id")
 	if err != nil {
 		return "", err
 	}
 
 	machineId := strings.Replace(string(data), "\n", "", -1)
-	instanceId := fmt.Sprintf("%s-%s-%s-%s-%s", 
+	instanceId := fmt.Sprintf("%s-%s-%s-%s-%s",
 		machineId[0:8],
 		machineId[8:12],
 		machineId[12:16],
 		machineId[16:20],
 		machineId[20:len(machineId)])
-		
+
 	return instanceId, nil
 }
 
-func (s *OpenStack) Mkfs(mountPoint string) {
-	//mkfs -t ext4 /dev/vdb
-	cmd := exec.Command("mkfs", "-t", "ext4", mountPoint)
-	fmt.Println(cmd)
+func (s *OpenStack) Mkfs(mountPoint string, fstype string) error {
+	cmd := exec.Command("mkfs", "-t", fstype, mountPoint)
 	var out bytes.Buffer
 	cmd.Stdout = &out
 	err := cmd.Run()
 	if err != nil {
-		fmt.Print(err)
+		return err
 	}
-	fmt.Printf("mkfs response: %s\n", out.String())
+	return nil
+	//fmt.Printf("mkfs response: %s\n", out.String())
 }
 
-func (s *OpenStack) GetVolume(token string, tenantId string, volumeId string) {
+func (s *OpenStack) GetVolume(token string, tenantId string, volumeId string) (*Volume, error) {
 	client := &http.Client{}
 	url := s.VolumesEndpoint + tenantId + "/volumes/" + volumeId
 	//fmt.Println(url)
@@ -101,125 +132,41 @@ func (s *OpenStack) GetVolume(token string, tenantId string, volumeId string) {
 	request.Header.Set("X-Auth-Token", token)
 	httpresp, httperr := client.Do(request)
 	if httperr != nil {
-		fmt.Println(httperr)
+		return nil, httperr
 	} else {
 		if httpresp.StatusCode == http.StatusOK {
 
 			data, err := ioutil.ReadAll(httpresp.Body)
 			if err != nil {
-				fmt.Print(err)
+				return nil, err
 			}
-				fmt.Print(string(data))
+
+			volume := Volume{}
 
 			volumeResp := make(map[string]interface{})
 			json.Unmarshal([]byte(data), &volumeResp)
 			//fmt.Print(volumeResp)
 			volumeMap := volumeResp["volume"].(map[string]interface{})
-			id := volumeMap["id"]
-			name := volumeMap["name"]
-			size := volumeMap["size"]
-			fmt.Printf("%s %s %f\n", id, name, size)
+
+			volume.Id = volumeMap["id"].(string)
+			volume.Name = volumeMap["name"].(string)
+			volume.Size = int(volumeMap["size"].(float64))
+
+			attachments := volumeMap["attachments"].([]interface{})
+			if attachments != nil && len(attachments) > 0 {
+				attachMap := attachments[0].(map[string]interface{})
+				volume.ServerId = attachMap["server_id"].(string)
+				volume.Device = attachMap["device"].(string)
+			}
+			return &volume, nil
 		} else {
 			fmt.Printf("Error %s\n", httpresp.Status)
 		}
 	}
+	return nil, nil
 }
 
-func (s *OpenStack) AttachVolumeCompute(token string, tenantId string, volumeId string, instanceId string) (bool, error){
-	fmt.Printf("Attaching %s to %s\n", volumeId, instanceId)
-
-	attachMap := map[string]interface{}{
-		"volumeId": volumeId,
-		"device": "/dev/vdb",
-	}
-	attachObj := map[string]interface{}{"volumeAttachment": attachMap}
-
-	data, _ := json.Marshal(attachObj)
-	fmt.Println(string(data))
-
-	client := &http.Client{}
-	url := s.ComputeEndpoint + tenantId + "/servers/" + instanceId + "/os-volume_attachments"
-	fmt.Println(url)
-	request, _ := http.NewRequest("POST", url, bytes.NewBuffer(data))
-	request.Header.Set("Content-Type", "application/json")
-	request.Header.Set("X-Auth-Token", token)
-	httpresp, httperr := client.Do(request)
-	if httperr != nil {
-		fmt.Println(httperr)
-		return false, httperr
-	} else {
-		if httpresp.StatusCode == http.StatusAccepted {
-			fmt.Print("Attached \n")
-			return true, nil
-		} else {
-			fmt.Println(httpresp.Status)
-			return false, nil
-		}
-	}
-	return false, nil
-}
-
-func (s *OpenStack) AttachVolume(token string, tenantId string, volumeId string, instanceId string, mountPoint string) (bool, error){
-	fmt.Printf("Attaching %s to %s", volumeId, instanceId)
-
-	attachMap := map[string]interface{}{
-		"instance_uuid": instanceId,
-		"mountpoint": mountPoint,
-	}
-	attachObj := map[string]interface{}{"os-attach": attachMap}
-
-	data, _ := json.Marshal(attachObj)
-	fmt.Println(string(data))
-
-	client := &http.Client{}
-	url := s.VolumesEndpoint + tenantId + "/volumes/" + volumeId + "/action"
-	request, _ := http.NewRequest("POST", url, bytes.NewBuffer(data))
-	request.Header.Set("Content-Type", "application/json")
-	request.Header.Set("X-Auth-Token", token)
-	httpresp, httperr := client.Do(request)
-	if httperr != nil {
-		fmt.Println(httperr)
-		return false, httperr
-	} else {
-		if httpresp.StatusCode == http.StatusAccepted {
-			fmt.Print("Attached \n")
-			return true, nil
-		} else {
-			fmt.Println(httpresp.Status)
-			return false, nil
-		}
-	}
-	return false, nil
-
-}
-
-func (s *OpenStack) DetachVolume(token string, tenantId string, volumeId string, instanceId string) {
-	detachMap := map[string]interface{}{
-		"instance_uuid": instanceId,
-		//"mountpoint": mountPoint,
-	}
-	detachObj := map[string]interface{}{"os-detach": detachMap}
-
-	data, _ := json.Marshal(detachObj)
-
-	client := &http.Client{}
-	url := s.VolumesEndpoint + tenantId + "/volumes/" + volumeId + "/action"
-	request, _ := http.NewRequest("POST", url, bytes.NewBuffer(data))
-	request.Header.Set("Content-Type", "application/json")
-	request.Header.Set("X-Auth-Token", token)
-	httpresp, httperr := client.Do(request)
-	if httperr != nil {
-		fmt.Println(httperr)
-	} else {
-		if httpresp.StatusCode == http.StatusAccepted {
-			fmt.Print("Detached \n")
-		} else {
-			fmt.Print(httpresp.Status)
-		}
-	}
-}
-
-func (s *OpenStack) CreateVolume(token string, tenantId string, name string, size int) (string, error){
+func (s *OpenStack) CreateVolume(token string, tenantId string, name string, size int) (string, error) {
 
 	volumeMap := map[string]interface{}{
 		"size":        size,
@@ -262,9 +209,10 @@ func (s *OpenStack) CreateVolume(token string, tenantId string, name string, siz
 	return "", nil
 }
 
-func (s *OpenStack) ListVolumes(tenantId string, token string) (map[string]string, error) {
+func (s *OpenStack) ListVolumes(tenantId string, token string) ([]Volume, error) {
 
-	volumes := make(map[string]string)
+	volumes := make([]Volume, 0)
+
 	client := &http.Client{}
 	url := s.VolumesEndpoint + tenantId + "/volumes"
 	request, _ := http.NewRequest("GET", url, nil)
@@ -272,7 +220,7 @@ func (s *OpenStack) ListVolumes(tenantId string, token string) (map[string]strin
 	request.Header.Set("X-Auth-Token", token)
 	httpresp, httperr := client.Do(request)
 	if httperr != nil {
-		fmt.Println(httperr)
+		return volumes, httperr
 	} else {
 		if httpresp.StatusCode == http.StatusOK {
 
@@ -281,19 +229,19 @@ func (s *OpenStack) ListVolumes(tenantId string, token string) (map[string]strin
 				return volumes, err
 			}
 
+			// Parse the response
 			volumeResp := make(map[string]interface{})
 			json.Unmarshal([]byte(data), &volumeResp)
 			volumesArray := volumeResp["volumes"].([]interface{})
 			for _, vol := range volumesArray {
-				id := vol.(map[string]interface{})["id"]
+				volume := Volume{}
+				volume.Id = vol.(map[string]interface{})["id"].(string)
 				name := vol.(map[string]interface{})["name"]
-				if name == nil {
-					volumes[id.(string)] = ""
-				} else {
-					volumes[id.(string)] = name.(string)
+				if name != nil {
+					volume.Name = name.(string)
 				}
+				volumes = append(volumes, volume)
 			}
-
 		} else {
 			fmt.Println("Error %s\n", httpresp.Status)
 		}
@@ -301,7 +249,7 @@ func (s *OpenStack) ListVolumes(tenantId string, token string) (map[string]strin
 	return volumes, nil
 }
 
-func (s *OpenStack) Authenticate(username string, password string, tenantId string) (string, error) {
+func (s *OpenStack) Authenticate(username string, password string, tenantId string) (*Token, error) {
 
 	fmt.Printf("Authenticate %s %s\n", username, tenantId)
 	client := &http.Client{}
@@ -315,45 +263,42 @@ func (s *OpenStack) Authenticate(username string, password string, tenantId stri
 	authObj := map[string]interface{}{"auth": authMap}
 
 	data, _ := json.Marshal(authObj)
-	//fmt.Println(string(data))
 	url := s.IdentityEndpoint + "/tokens"
-	//fmt.Println(url)
+
 	request, _ := http.NewRequest("POST", url, bytes.NewBuffer(data))
 	request.Header.Set("Content-Type", "application/json")
 	httpresp, httperr := client.Do(request)
 	if httperr != nil {
 		fmt.Println(httperr)
-		return "", httperr
+		return nil, httperr
 	} else {
 		if httpresp.StatusCode == http.StatusOK {
 
 			data, err := ioutil.ReadAll(httpresp.Body)
 			if err != nil {
-				return "", err
+				return nil, err
 			}
 			tokenResp := make(map[string]interface{})
 			json.Unmarshal([]byte(data), &tokenResp)
 			tokenMap := tokenResp["access"].(map[string]interface{})["token"].(map[string]interface{})
-			token := tokenMap["id"].(string)
-			return token, nil
+
+			token := Token{}
+			token.Id = tokenMap["id"].(string)
+			token.Expires, err = time.Parse(time.RFC3339, tokenMap["expires"].(string))
+			if err != nil {
+				return nil, err
+			}
+
+			return &token, nil
 		} else {
 			fmt.Println("Error %s\n", httpresp.Status)
 		}
 	}
-	return "", nil
-}
-
-
-
-type Attachment struct {
-	id string
-	device string
-	serverId string
-	volumeId string
+	return nil, nil
 }
 
 // DELETE powervc/openstack/compute/v2/{tenant_id}/servers/{server_id}/os-volume_attachments/{attachment_id}
-func (s *OpenStack) deleteVolumeAttachment(token string, tenantId string, instanceId string, attachmentId string) {
+func (s *OpenStack) DetachVolume(token string, tenantId string, instanceId string, attachmentId string) error {
 
 	client := &http.Client{}
 	url := s.ComputeEndpoint + tenantId + "/servers/" + instanceId + "/os-volume_attachments/" + attachmentId
@@ -363,7 +308,7 @@ func (s *OpenStack) deleteVolumeAttachment(token string, tenantId string, instan
 	request.Header.Set("X-Auth-Token", token)
 	httpresp, httperr := client.Do(request)
 	if httperr != nil {
-		fmt.Println(httperr)
+		return httperr
 	} else {
 		if httpresp.StatusCode == http.StatusOK {
 			fmt.Printf("Deleted %s\n", httpresp.Status)
@@ -371,13 +316,13 @@ func (s *OpenStack) deleteVolumeAttachment(token string, tenantId string, instan
 			fmt.Printf("Error %s\n", httpresp.Status)
 		}
 	}
+	return nil
 }
 
-
 // GET 	powervc/openstack/compute/v2/{tenant_id}/servers/{server_id}/os-volume_attachments
-func (s *OpenStack) getVolumeAttachments(token string, tenantId string, instanceId string) ([]Attachment, error ){
+func (s *OpenStack) GetVolumeAttachments(token string, tenantId string, instanceId string) ([]Attachment, error) {
 
-	attachments := make([]Attachment,0)
+	attachments := make([]Attachment, 0)
 	client := &http.Client{}
 	url := s.ComputeEndpoint + tenantId + "/servers/" + instanceId + "/os-volume_attachments"
 	request, _ := http.NewRequest("GET", url, nil)
@@ -394,6 +339,7 @@ func (s *OpenStack) getVolumeAttachments(token string, tenantId string, instance
 				return attachments, err
 			}
 
+			fmt.Println(string(data))
 			attachResp := make(map[string]interface{})
 			json.Unmarshal([]byte(data), &attachResp)
 			attachArray := attachResp["volumeAttachments"].([]interface{})
@@ -404,7 +350,7 @@ func (s *OpenStack) getVolumeAttachments(token string, tenantId string, instance
 				attachment.serverId = attach.(map[string]interface{})["serverId"].(string)
 				attachment.volumeId = attach.(map[string]interface{})["volumeId"].(string)
 				attachments = append(attachments, attachment)
-	
+
 			}
 
 		} else {
@@ -414,14 +360,12 @@ func (s *OpenStack) getVolumeAttachments(token string, tenantId string, instance
 	return attachments, nil
 }
 
-
-
 // POST powervc/openstack/compute/v2/{tenant_id}/servers/{server_id}/os-volume_attachments/{attachment_id}
-func (s *OpenStack) createVolumeAttachment(token string, tenantId string, instanceId string, volumeId string, mountPoint string) (error ){
+func (s *OpenStack) AttachVolume(token string, tenantId string, instanceId string, volumeId string, mountPoint string) error {
 	attachMap := map[string]interface{}{
 		"serverId": instanceId,
 		"volumeId": volumeId,
-		"device": mountPoint,
+		"device":   mountPoint,
 	}
 	attachObj := map[string]interface{}{"volumeAttachment": attachMap}
 
@@ -443,5 +387,27 @@ func (s *OpenStack) createVolumeAttachment(token string, tenantId string, instan
 			fmt.Printf("Error %s\n", httpresp.Status)
 		}
 	}
-	return  nil
+	return nil
+}
+
+func (s *OpenStack) DeleteVolume(token string, tenantId string, volumeId string) error {
+
+	client := &http.Client{}
+	url := s.VolumesEndpoint + tenantId + "/volumes/" + volumeId
+	request, _ := http.NewRequest("DELETE", url, nil)
+	request.Header.Set("Content-Type", "application/json")
+	request.Header.Set("X-Auth-Token", token)
+	httpresp, httperr := client.Do(request)
+	if httperr != nil {
+		return httperr
+	} else {
+		if httpresp.StatusCode == http.StatusAccepted {
+			fmt.Printf("Success %s\n", httpresp.Status)
+			return nil
+		} else {
+			fmt.Printf("Error %s\n", httpresp.Status)
+			return nil
+		}
+	}
+	return nil
 }
