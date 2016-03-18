a58ec8498d87f20471e7d67025d13b17475e6cb8
Craig Willis <willis8@illinois.edu>
2016-03-18 17:11:11 -0500

    Added register path

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@
 cd /go/src/github.com/nds-labs/apiserver
 go get
 #go build -ldflags "-X main.Version=0.1alpha -X main.BuildDate=`date "+%Y-%m-%dT%H:%M:%S"`"
-go build -ldflags "-X main.Version=0.1alpha -X main.BuildDate=`date "+%Y-%m-%dT%H:%M:%S"`"
+go build 
