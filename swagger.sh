df9ce930af2976f42053e32044f2450655616c4a
Mike Lambert <lambert8@illinois.edu>
2016-03-22 19:50:07 +0000

    Restructuring swagger stuff

--- swagger.sh
+++ swagger.sh
@@ -4,7 +4,7 @@
 
 DOWNLOADURL=$1
 SWAGGERFILE=ndslabs.json
-OUTPUTFILE=../src/app/shared/api.js
+OUTPUTFILE=src/app/shared/api.js
 
 # Install swagger-js-codegen
 docker run --rm -it -v `pwd`/data bodom0015/nodejs-bower-grunt npm install swagger-js-codegen
@@ -14,12 +14,12 @@ if [[ "${@/-u/}" != "$@" ]]; then
 	echo "Downloading newest Swagger Spec..."
 	sudo curl -L ${DOWNLOADURL} > ${SWAGGERFILE}
 else
-	echo "Rebuilding swagger spec from ../../apis/swagger-spec/ndslabs.json"
-	sudo cp ../../apis/swagger-spec/ndslabs.json ./ndslabs.json
+	echo "Rebuilding swagger spec from ../apis/swagger-spec/ndslabs.json"
+	sudo cp ../apis/swagger-spec/ndslabs.json api/ndslabs.json
 fi
 
 # Finally, generate AngularJS source
 echo "Generating AngularJS source code..."
-docker run --rm -it -v `pwd`:/data bodom0015/nodejs-bower-grunt node generate-angular.js > ${OUTPUTFILE} || cat ${OUTPUTFILE}
+docker run --rm -it -v `pwd`/api:/data bodom0015/nodejs-bower-grunt node generate-angular.js > ${OUTPUTFILE} || cat ${OUTPUTFILE}
 
 echo "Done!"
