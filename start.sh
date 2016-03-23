6722918931fa4598aeec69d24e097121db1ea233
Mike Lambert <lambert8@illinois.edu>
2016-03-23 21:01:53 +0000

    Updated GUI docker image structure

--- start.sh
+++ start.sh
@@ -16,12 +16,10 @@ WEBSERVER_PORT=30000
 APISERVER_HOST=141.142.208.127
 APISERVER_PORT=30001
 
-JSSRCDIR=`pwd`/js
-
 # If -d or --dev specified, mount directory for dynamically testing changes
 OPTS=
 if [[ "${@/-d/}" != "$@" || "${@/--dev/}" != "$@" ]]; then
-	OPTS="-v ${JSSRCDIR}:/home/app"
+	OPTS="-v `pwd`:/home/app"
 fi
 
 # If -c specified, just open a console
@@ -35,6 +33,6 @@ docker run --name ndslabs-gui -it -d -p ${WEBSERVER_PORT}:8080 -e APISERVER_HOST
 
 # If -d or --dev specified, run a Cloud 9 Docker container
 if [[ "$OPTS" != "" ]]; then
-	docker run --name cloud9 -it -d -p ${CLOUD9_PORT}:80 -v ${JSSRCDIR}:/workspace bodom0015/cloud9 && \
+	docker run --name cloud9 -it -d -p ${CLOUD9_PORT}:80 -v `pwd`:/workspace bodom0015/cloud9 && \
 	echo "You should now be able to access Cloud 9 by navigating to http://{YOUR_FLOATING_IP}:1234."
 fi
