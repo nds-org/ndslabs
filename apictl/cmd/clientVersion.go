8dfd2d867c5dd79e7132fa342f3fc80e8d7cca5a
Craig Willis <willis8@illinois.edu>
2016-03-25 17:03:01 -0500

    Updated version

--- apictl/cmd/clientVersion.go
+++ apictl/cmd/clientVersion.go
@@ -1,7 +1,7 @@
 package cmd
 const (
   VERSION = "0.1alpha"
-  BUILD_DATE = "2016-03-24 14:01"
-  GIT_COMMIT = "153a3f51bc442c6099f88ddc7d84c2bed68f787c"
+  BUILD_DATE = "2016-03-25 08:57"
+  GIT_COMMIT = "d2762e56e126d40b9a9188f998f111a76a5b19ef"
   GIT_TAG = ""
 )
