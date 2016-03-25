30d814520d890dadf143e400cf6d452168c280ea
Mike Lambert <lambert8@illinois.edu>
2016-03-25 04:10:02 +0000

    Finally got these changes merged back in

--- start.sh
+++ start.sh
@@ -13,7 +13,7 @@ CLOUD9_USER=cloud9
 CLOUD9_PASS=iHeartNdsLabs
 
 WEBSERVER_PORT=30000
-APISERVER_HOST=141.142.208.127
+APISERVER_HOST=192.168.99.100
 APISERVER_PORT=30001
 
 # If -d or --dev specified, mount directory for dynamically testing changes
@@ -34,5 +34,5 @@ docker run --name ndslabs-gui -it -d -p ${WEBSERVER_PORT}:8080 -e APISERVER_HOST
 # If -d or --dev specified, run a Cloud 9 Docker container
 if [[ "$OPTS" != "" ]]; then
 	docker run --name cloud9 -it -d -p ${CLOUD9_PORT}:80 -v `pwd`:/workspace bodom0015/cloud9 && \
-	echo "You should now be able to access Cloud 9 by navigating to http://{YOUR_FLOATING_IP}:1234."
+	echo "You should now be able to access Cloud 9 by navigating to http://{YOUR_FLOATING_IP}:${CLOUD9_PORT}."
 fi
