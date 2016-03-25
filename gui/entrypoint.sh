d6d060df633ca5d00e0406862f6f545cd800173d
Mike Lambert <lambert8@illinois.edu>
2016-03-25 08:44:26 +0000

    Sync for new image / entrypoint fix

--- gui/entrypoint.sh
+++ gui/entrypoint.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 
-cd js/
-
-# Install dependencies
-npm install -g http-server bower grunt
-npm install
-bower install --config.interactive=false --allow-root
-
 # Substitute the APISERVER_HOST and PORT passed in by "docker run -e"
-/bin/sed -i -e "s#^\.constant('ApiHost', '.*')#.constant('ApiHost', '${APISERVER_HOST}')#" "app/app.js"
-/bin/sed -i -e "s#^\.constant('ApiPort', '.*')#.constant('ApiPort', '${APISERVER_PORT}')#" "app/app.js"
+/bin/sed -i -e "s#^\.constant('ApiHost', '.*')#.constant('ApiHost', '${APISERVER_HOST}')#" "js/app/app.js"
+/bin/sed -i -e "s#^\.constant('ApiPort', '.*')#.constant('ApiPort', '${APISERVER_PORT}')#" "js/app/app.js"
 
 # Start our HTTP Server
 http-server /home/app/js
