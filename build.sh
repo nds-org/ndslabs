361792ad6488dfb49ea505b1da3d27191e06ed60
Craig Willis <willis8@illinois.edu>
2016-03-25 10:17:14 -0500

    Changed binary build path

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@
 cd /go/src/github.com/ndslabs/apiserver
 go get
 #go build -ldflags "-X main.Version=0.1alpha -X main.BuildDate=`date "+%Y-%m-%dT%H:%M:%S"`"
-GOOS=linux GOARCH=amd64 go build -o build/bin/amd64/apiserver
+GOOS=linux GOARCH=amd64 go build -o build/bin/apiserver-linux-amd64
