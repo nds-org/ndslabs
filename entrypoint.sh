e9645a5fdf93840c790b649641f6509dd2966bd2
Mike Lambert <lambert8@illinois.edu>
2016-03-26 20:41:59 +0000

    Fixed docker image build

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Substitute the APISERVER_HOST and PORT passed in by "docker run -e"
-/bin/sed -i -e "s#^\.constant('ApiHost', '.*')#.constant('ApiHost', '${APISERVER_HOST}')#" "js/app/app.js"
-/bin/sed -i -e "s#^\.constant('ApiPort', '.*')#.constant('ApiPort', '${APISERVER_PORT}')#" "js/app/app.js"
+/bin/sed -i -e "s#^\.constant('ApiHost', '.*')#.constant('ApiHost', '${APISERVER_HOST}')#" "/home/app/js/app/app.js"
+/bin/sed -i -e "s#^\.constant('ApiPort', '.*')#.constant('ApiPort', '${APISERVER_PORT}')#" "/home/app/js/app/app.js"
 
 # Start our HTTP Server
 http-server /home/app/js
