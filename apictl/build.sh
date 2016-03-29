a7099d8d41bf3ed24abe7b97d95f66f905419a74
Craig Willis <willis8@illinois.edu>
2016-03-29 16:42:26 -0500

    Removed Makefile -> build.sh

--- apictl/build.sh
+++ apictl/build.sh
@@ -1,5 +1,28 @@
 #!/bin/bash
 
-cd /go/src/github.com/ndslabs/apictl
-go get
-GOOS=linux GOARCH=amd64 go build -o build/bin/ndslabsctl-linux-amd64
+BUILD_DATE=`date +%Y-%m-%d\ %H:%M`
+VERSIONFILE="cmd/clientVersion.go"
+VERSION="1.0-alpha"
+APP="ndslabsctl"
+
+if [ "$1" = "build" ] || [ -z $1 ]; then
+	echo Building $APP
+	rm -f $VERSIONFILE
+	echo "package cmd" > $VERSIONFILE
+	echo "const (" >> $VERSIONFILE
+	echo "  VERSION = \"1.0-alpha\"" >> $VERSIONFILE
+	echo "  BUILD_DATE = \"$BUILD_DATE\"" >> $VERSIONFILE
+	echo ")" >> $VERSIONFILE
+	rm -f build/bin/$APP-linux-amd64 build/$APP-darwin-amd64
+	mkdir -p build/bin build/bin build/pkg
+	echo Building Darwin
+	GOOS=darwin GOARCH=amd64 go build -o build/bin/$APP-darwin-amd64
+	echo Building Linux
+	docker run --rm -it -v `pwd`/../apiserver:/go/src/github.com/ndslabs/apiserver -v `pwd`:/go/src/github.com/ndslabs/apictl -v `pwd`/build/bin:/go/bin -v `pwd`/build/pkg:/go/pkg -v `pwd`/gobuild.sh:/gobuild.sh golang /gobuild.sh
+elif [ "$1" = "clean" ]; then
+	echo Cleaning
+	rm -rf build
+fi
+
+
+
--- apictl/cmd/clientVersion.go
+++ apictl/cmd/clientVersion.go
@@ -1,7 +1,5 @@
 package cmd
 const (
   VERSION = "1.0-alpha"
-  BUILD_DATE = "2016-03-28 17:18"
-  GIT_COMMIT = "f5f26043a14e8e553f6bd41ee3af0886a1bddf78"
-  GIT_TAG = ""
+  BUILD_DATE = "2016-03-29 16:36"
 )
