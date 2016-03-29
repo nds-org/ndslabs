525fcf8c35ca722ae50dff9d61d294a41164a4c9
Craig Willis <willis8@illinois.edu>
2016-03-29 16:43:58 -0500

    Removed Makefile -> build.sh

--- apiserver/build.sh
+++ apiserver/build.sh
@@ -1,6 +1,45 @@
 #!/bin/bash
 
-cd /go/src/github.com/ndslabs/apiserver
-go get
-#go build -ldflags "-X main.Version=0.1alpha -X main.BuildDate=`date "+%Y-%m-%dT%H:%M:%S"`"
-GOOS=linux GOARCH=amd64 go build -o build/bin/apiserver-linux-amd64
+BUILD_DATE=`date +%Y-%m-%d\ %H:%M`
+VERSIONFILE="version.go"
+VERSION="1.0-alpha"
+APP="apiserver"
+
+if [ "$1" = "build" ] || [ -z $1 ]; then
+	echo Building $APP
+	rm $VERSIONFILE
+	echo "package main" > $VERSIONFILE
+	echo "const (" >> $VERSIONFILE
+	echo "  VERSION = \"$VERSION\"" >> $VERSIONFILE
+	echo "  BUILD_DATE = \"$BUILD_DATE\"" >> $VERSIONFILE
+	echo ")" >> $VERSIONFILE
+	rm -f build/bin/$APP-linux-amd64 build/$APP-darwin-amd64
+	mkdir -p build/bin build/bin build/pkg
+	echo Building Darwin
+	GOOS=darwin GOARCH=amd64 go build -o build/bin/$APP-darwin-amd64
+	echo Building Linux
+	docker run --rm -it -v `pwd`:/go/src/github.com/ndslabs/apiserver -v `pwd`/build/bin:/go/bin -v `pwd`/build/pkg:/go/pkg -v `pwd`/gobuild.sh:/gobuild.sh golang  /gobuild.sh
+
+elif [ "$1" = "dev" ]; then
+	echo Building dev image
+	docker build -t ndslabs/apiserver:dev .
+	echo Pushing dev image
+	docker push ndslabs/apiserver:dev
+elif [ "$1" = "test" ]; then
+	echo Building test image
+	docker build -t ndslabs/apiserver:test .
+	echo Pushing test image
+	docker push ndslabs/apiserver:test
+elif [ "$1" = "release" ]; then
+	echo Building release image
+	docker build -t ndslabs/apiserver:latest .
+	docker build -t ndslabs/apiserver:$(VERSION) .
+	echo Pushing release image
+	docker push ndslabs/apiserver:latest
+	docker push ndslabs/apiserver:$(VERSION)
+elif [ "$1" = "clean" ]; then
+	echo Cleaning
+	rm -rf build
+fi
+
+
--- apiserver/version.go
+++ apiserver/version.go
@@ -1,5 +1,5 @@
 package main
 const (
   VERSION = "1.0-alpha"
-  BUILD_DATE = "2016-03-28 16:49"
+  BUILD_DATE = "2016-03-29 16:42"
 )
