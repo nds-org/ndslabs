a741cb018016a9b7e3cd1c74d37b9d42d49a2743
Mike Lambert <lambert8@illinois.edu>
2016-04-26 17:10:44 +0000

    Fix some wiring issues

--- js/app/expert/modals/addService/AddServiceController.js
+++ js/app/expert/modals/addService/AddServiceController.js
@@ -8,18 +8,23 @@ angular
  * @author lambert8
  * @see https://opensource.ncsa.illinois.edu/confluence/display/~lambert8/3.%29+Controllers%2C+Scopes%2C+and+Partial+Views
  */
-.controller('AddServiceCtrl', [ '$scope', '$log', '$uibModalInstance', '_', 'ServiceDiscovery', 'Project', 'stack', 'service', 
-    function($scope, $log, $uibModalInstance, _, ServiceDiscovery, stack, service) {
-  // Configuration
-  $scope.extraConfigs = ServiceDiscovery.discoverConfigRequirements(stack, service);
-  
+.controller('AddServiceCtrl', [ '$scope', '$log', '$uibModalInstance', '_', 'ServiceDiscovery', 'Project', 'Specs', 'stack', 'service', 
+    function($scope, $log, $uibModalInstance, _, ServiceDiscovery, Project, Specs, stack, service) {
+  var wrapper = { 
+    services: [ service ]
+  };
+      
   // Volumes
-  $scope.newStackVolumeRequirements = ServiceDiscovery.discoverRequiredVolumes(stack, service);
-  $scope.newStackOrphanedVolumes = ServiceDiscovery.discoverOrphanVolumes(stack, service);
+  $scope.storageQuota = Project.project.storageQuota;
+  $scope.newStackVolumeRequirements = ServiceDiscovery.discoverRequiredVolumes(wrapper);
+  $scope.newStackOrphanedVolumes = ServiceDiscovery.discoverOrphanVolumes(wrapper);
   
-  $scope.ok = function(removeVolumes) {
+  // Configuration
+  $scope.extraConfig = ServiceDiscovery.discoverConfigRequirements(wrapper);
+  
+  $scope.ok = function(service) {
     $log.debug("Closing modal with success!");
-    $uibModalInstance.close(removeVolumes);
+    $uibModalInstance.close(service);
   };
   
   $scope.close = function() {
