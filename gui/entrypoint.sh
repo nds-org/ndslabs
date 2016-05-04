26d2894764e02479ca7eb3f794288e5133d8bac8
Mike Lambert <lambert8@illinois.edu>
2016-05-04 17:56:14 -0500

    Update entrypoint.sh

--- gui/entrypoint.sh
+++ gui/entrypoint.sh
@@ -3,6 +3,7 @@
 # Substitute the APISERVER_HOST and PORT passed in by "docker run -e"
 /bin/sed -i -e "s#^\.constant('ApiHost', '.*')#.constant('ApiHost', '${APISERVER_HOST}')#" "/home/app/js/app/app.js"
 /bin/sed -i -e "s#^\.constant('ApiPort', '.*')#.constant('ApiPort', '${APISERVER_PORT}')#" "/home/app/js/app/app.js"
+/bin/sed -i -e "s#^\.constant('ApiPath', '.*')#.constant('ApiPath', '${APISERVER_PATH}')#" "/home/app/js/app/app.js"
 
 # Start our HTTP Server
 http-server /home/app/js
