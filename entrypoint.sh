2437feed8ec96107b3ddf534f739627466ff208a
Mike Lambert <lambert8@illinois.edu>
2016-05-05 19:15:04 +0000

    Added an awful hack that should be removed/fixed by implementing a build chain for the UI

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,9 @@
 /bin/sed -i -e "s#^\.constant('ApiPort', '.*')#.constant('ApiPort', '${APISERVER_PORT}')#" "/home/app/js/app/app.js"
 /bin/sed -i -e "s#^\.constant('ApiPath', '.*')#.constant('ApiPath', '${APISERVER_PATH}')#" "/home/app/js/app/app.js"
 
+# Fix this awful hack
+cp -r js/ ui/
+mv ui/ js/
+
 # Start our HTTP Server
 http-server /home/app/js
