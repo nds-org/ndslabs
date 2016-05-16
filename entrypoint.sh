90219f198e5f6eedbf3d641f992eb45331f21680
Mike Lambert <lambert8@illinois.edu>
2016-05-16 02:12:53 +0000

    Parameterize UI base path and new build date

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 # Substitute the APISERVER_HOST and PORT passed in by "docker run -e"
-/bin/sed -i -e "s#^\.constant('ApiHost', '.*')#.constant('ApiHost', '${APISERVER_HOST}')#" "/home/app/js/app/app.js"
-/bin/sed -i -e "s#^\.constant('ApiPort', '.*')#.constant('ApiPort', '${APISERVER_PORT}')#" "/home/app/js/app/app.js"
-/bin/sed -i -e "s#^\.constant('ApiPath', '.*')#.constant('ApiPath', '${APISERVER_PATH}')#" "/home/app/js/app/app.js"
+/bin/sed -i -e "s#^\.constant('ApiHost', '.*')#.constant('ApiHost', '${APISERVER_HOST}')#" "/home/js/app/app.js"
+/bin/sed -i -e "s#^\.constant('ApiPort', '.*')#.constant('ApiPort', '${APISERVER_PORT}')#" "/home/js/app/app.js"
+/bin/sed -i -e "s#^\.constant('ApiPath', '.*')#.constant('ApiPath', '${APISERVER_PATH}')#" "/home/js/app/app.js"
 
-# TODO: Fix this awful hack
-cp -r js/ ui/
-mv ui/ js/
+# Move contents to the appropriate base path
+# TODO: Is this necessary?
+mkdir -p /home/$UI_BASE_PATH
+mv js/* /home/$UI_BASE_PATH
 
 # Start our HTTP Server
-http-server /home/app/js
+http-server /home/
