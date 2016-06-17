79c990b1f4b76d9ea2b67d026f1804754b7676f8
craig-willis <willis8@illinois.edu>
2016-06-17 11:11:56 -0500

    NDS-314: Default resources limits and Godeps (#62)
    * Increased build number
    
    * Added VOLUME_PATH env var
    
    * Added much-needed godep support
    
    * Added default resource limits
    
    * Changed version

--- gobuild.sh
+++ gobuild.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cd /go/src/github.com/ndslabs/apiserver
-go get
+go get github.com/tools/godep
 #go build -ldflags "-X main.Version=0.1alpha -X main.BuildDate=`date "+%Y-%m-%dT%H:%M:%S"`"
-GOOS=linux GOARCH=amd64 go build -o build/bin/apiserver-linux-amd64
+GOOS=linux GOARCH=amd64 godep go build -o build/bin/apiserver-linux-amd64
