8fa0daeb966effeaca47c4cd9f322e6e670177f8
Craig Willis <willis8@illinois.edu>
2016-07-26 14:59:20 -0500

    Added apictl to travis build

--- apictl/build.sh
+++ apictl/build.sh
@@ -5,17 +5,25 @@ VERSIONFILE="cmd/clientVersion.go"
 VERSION="1.0.1-alpha"
 APP="ndslabsctl"
 
-if [ "$1" = "build" ] || [ -z $1 ]; then
-	echo Building $APP
-	if [ -e "$VERSIONFILE" ]; then
-		rm -f $VERSIONFILE
-	fi
-	echo "package cmd" > $VERSIONFILE
-	echo "const (" >> $VERSIONFILE
-	echo "  VERSION = \"1.0-alpha\"" >> $VERSIONFILE
-	echo "  BUILD_DATE = \"$BUILD_DATE\"" >> $VERSIONFILE
-	echo ")" >> $VERSIONFILE
-	docker run --rm -it -v `pwd`/../apiserver:/go/src/github.com/ndslabs/apiserver -v `pwd`:/go/src/github.com/ndslabs/apictl -v `pwd`/build/bin:/go/bin -v `pwd`/build/pkg:/go/pkg -v `pwd`/gobuild.sh:/gobuild.sh golang:1.6 /gobuild.sh
+
+if [ "$1" = "local" ] || [ "$1" = "docker" ]; then
+    echo Building $APP
+    if [ -e "$VERSIONFILE" ]; then
+        rm $VERSIONFILE
+    fi
+    echo "package main" > $VERSIONFILE
+    echo "const (" >> $VERSIONFILE
+    echo "  VERSION = \"$VERSION\"" >> $VERSIONFILE
+    echo "  BUILD_DATE = \"$BUILD_DATE\"" >> $VERSIONFILE
+    echo ")" >> $VERSIONFILE
+    if [ "$1" = "local" ]; then
+        glide install
+        go build
+    fi
+    if [ "$1" = "docker" ]; then
+		docker run --rm -it -v `pwd`/../apiserver:/go/src/github.com/ndslabs/apiserver -v `pwd`:/go/src/github.com/ndslabs/apictl -v `pwd`/build/bin:/go/bin -v `pwd`/build/pkg:/go/pkg -v `pwd`/gobuild.sh:/gobuild.sh golang:1.6 /gobuild.sh
+    fi
+
 	rm $VERSIONFILE
 elif [ "$1" = "clean" ]; then
 	echo Cleaning
