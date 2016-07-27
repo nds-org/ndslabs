923da71b2ebc438fdbffa79a547bdf7b2ff07745
Craig Willis <willis8@illinois.edu>
2016-07-27 10:45:55 -0500

    Fixed build

--- apiserver/build.sh
+++ apiserver/build.sh
@@ -15,9 +15,18 @@ if [ "$1" = "local" ] || [ "$1" = "docker" ]; then
 	echo "  VERSION = \"$VERSION\"" >> $VERSIONFILE
 	echo "  BUILD_DATE = \"$BUILD_DATE\"" >> $VERSIONFILE
 	echo ")" >> $VERSIONFILE
-	if [ "$1" = "local" ]; then 
-		glide install 
-		go build
+	if f[ "$1" = "local" ]; then 
+        glide install
+
+        UNAME=$(uname)
+
+        if [ "$UNAME" == "Darwin" ]; then
+            echo Building darwin-amd64
+            GOOS=darwin GOARCH=amd64 go build -o build/bin/$APP-darwin-amd64
+        elif [ "$UNAME" == "Linux" ]; then
+            echo Building linux-amd64
+            GOOS=linux GOARCH=amd64 go build -o build/bin/$APP-linux-amd64
+        fi
 	fi
 	if [ "$1" = "docker" ]; then 
 		docker run --rm -it -v `pwd`:/go/src/github.com/ndslabs/apiserver -v `pwd`/build/bin:/go/bin -v `pwd`/build/pkg:/go/pkg -v `pwd`/gobuild.sh:/gobuild.sh golang:1.6  /gobuild.sh
