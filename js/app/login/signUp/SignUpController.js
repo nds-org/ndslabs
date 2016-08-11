099cf1240e3f7fe5ffa5f593ee09ad62e8f0ef84
Mike Lambert <lambert8@illinois.edu>
2016-08-11 14:06:53 -0500

    NDS-255: Passwords should not be returned in plaintext (#102)

--- js/app/login/signUp/SignUpController.js
+++ js/app/login/signUp/SignUpController.js
@@ -8,20 +8,19 @@ angular
  * @author lambert8
  * @see https://opensource.ncsa.illinois.edu/confluence/display/~lambert8/3.%29+Controllers%2C+Scopes%2C+and+Partial+Views
  */
-.controller('SignUpController', [ '$scope', '$uibModalInstance', '$log', 'NdsLabsApi', function($scope, $uibModalInstance, $log, NdsLabsApi) {
-  $scope.newProject = {
-    name: '',
-    description: '',
-    namespace: '',
-    password: '',
-    passwordConfirmation: ''
-  };
+.controller('SignUpController', [ '$scope', '$uibModalInstance', '$log', 'NdsLabsApi', 'Project', function($scope, $uibModalInstance, $log, NdsLabsApi, Project) {
+  $scope.newProject = Project.create();
   
-  $scope.ok = function(project) {
-    NdsLabsApi.postRegister({ 'account': project }).then(function(data, xhr) {
-      $uibModalInstance.close(data);
+  $scope.ok = function(account) {
+    NdsLabsApi.postRegister({ 'account': account }).then(function(data, xhr) {
+      var user = account.namespace;
+      var pass = account.password;
+      
+      // Clear out sensitive data
+      $scope.newProject = Project.create();
+      $uibModalInstance.close({ 'username': user, 'password': pass });
     }, function(headers) {
-      $log.error('Failed to create account: ' + project.namespace);
+      $log.error('Failed to create account: ' + account.namespace);
     });
   };
   
