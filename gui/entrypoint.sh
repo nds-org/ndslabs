d3982e390809dd47a7152ece490c3ade954ffc4c
Mike Lambert <lambert8@illinois.edu>
2016-08-15 22:42:21 +0000

    NDS-444: UI build date no longer updates automatically

--- gui/entrypoint.sh
+++ gui/entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Substitute the APISERVER_HOST and PORT passed in by "docker run -e"
-/bin/sed -i -e "s#^\.constant('ApiHost', '.*')#.constant('ApiHost', '${APISERVER_HOST}')#" "/home/js/app/app.js"
-/bin/sed -i -e "s#^\.constant('ApiPort', '.*')#.constant('ApiPort', '${APISERVER_PORT}')#" "/home/js/app/app.js"
-/bin/sed -i -e "s#^\.constant('ApiPath', '.*')#.constant('ApiPath', '${APISERVER_PATH}')#" "/home/js/app/app.js"
-/bin/sed -i -e "s#^\.constant('ApiSecure', .*)#.constant('ApiSecure', ${APISERVER_SECURE})#" "/home/js/app/app.js"
+/bin/sed -i -e "s#^\.constant('ApiHost', '.*')#.constant('ApiHost', '${APISERVER_HOST:-192.168.99.100}')#" "/home/js/app/app.js"
+/bin/sed -i -e "s#^\.constant('ApiPort', '.*')#.constant('ApiPort', '${APISERVER_PORT:-30001}')#" "/home/js/app/app.js"
+/bin/sed -i -e "s#^\.constant('ApiPath', '.*')#.constant('ApiPath', '${APISERVER_PATH:-/api}')#" "/home/js/app/app.js"
+/bin/sed -i -e "s#^\.constant('ApiSecure', .*)#.constant('ApiSecure', ${APISERVER_SECURE:-false})#" "/home/js/app/app.js"
 
 # Start our HTTP Server
 http-server /home/js/
