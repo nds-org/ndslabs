b23abbac8131b99cdfa672d30454fbea9d57ed7c
Mike Lambert <lambert8@illinois.edu>
2016-08-17 17:53:16 +0000

    NDS-213 / NDS-285

--- js/app/login/signUp/SignUpController.js
+++ js/app/login/signUp/SignUpController.js
@@ -10,17 +10,27 @@ angular
  */
 .controller('SignUpController', [ '$scope', '$uibModalInstance', '$log', 'NdsLabsApi', 'Project', function($scope, $uibModalInstance, $log, NdsLabsApi, Project) {
   $scope.newProject = Project.create();
+  $scope.progressMessage = '';
   
   $scope.ok = function(account) {
+    $scope.progressMessage = 'Please wait...';
+    $scope.errorMessage = '';
+    
     NdsLabsApi.postRegister({ 'account': account }).then(function(data, xhr) {
       var user = account.namespace;
       var pass = account.password;
       
+      $scope.errorMessage = '';
+      
       // Clear out sensitive data
       $scope.newProject = Project.create();
       $uibModalInstance.close({ 'username': user, 'password': pass });
-    }, function(headers) {
+    }, function(response) {
       $log.error('Failed to create account: ' + account.namespace);
+      $scope.errorMessage = 'Username or e-mail address already in use';
+      $log.error("Error logging in!");
+    }).finally(function() {
+      $scope.progressMessage = '';
     });
   };
   
