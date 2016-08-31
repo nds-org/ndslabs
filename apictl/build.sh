b1bd97a94ab6aa7b3468800ec69a030cd446634f
Craig Willis <willis8@illinois.edu>
2016-08-31 17:27:48 -0500

    Added top-level Dockerfile to build the api server with the CLI included along with download endpoint

--- apictl/build.sh
+++ apictl/build.sh
@@ -6,38 +6,21 @@ VERSION="1.0.2"
 APP="ndslabsctl"
 
 
-if [ "$1" = "local" ] || [ "$1" = "docker" ]; then
-    echo Building $APP
-    if [ -e "$VERSIONFILE" ]; then
-        rm $VERSIONFILE
-    fi
-    echo "package cmd" > $VERSIONFILE
-    echo "const (" >> $VERSIONFILE
-    echo "  VERSION = \"$VERSION\"" >> $VERSIONFILE
-    echo "  BUILD_DATE = \"$BUILD_DATE\"" >> $VERSIONFILE
-    echo ")" >> $VERSIONFILE
-    if [ "$1" = "local" ]; then
-        glide install
-
-		UNAME=$(uname)
-
-		if [ "$UNAME" == "Darwin" ]; then
-    		echo Building darwin-amd64
-			GOOS=darwin GOARCH=amd64 go build -o build/bin/$APP-darwin-amd64
-		elif [ "$UNAME" == "Linux" ]; then
-    		echo Building linux-amd64
-  	 	 	GOOS=linux GOARCH=amd64 go build -o build/bin/$APP-linux-amd64
-		fi
-    fi
-    if [ "$1" = "docker" ]; then
-		docker run --rm -it -v `pwd`/../apiserver:/go/src/github.com/ndslabs/apiserver -v `pwd`:/go/src/github.com/ndslabs/apictl -v `pwd`/build/bin:/go/bin -v `pwd`/build/pkg:/go/pkg -v `pwd`/gobuild.sh:/gobuild.sh golang:1.6 /gobuild.sh
-    fi
-
-	rm $VERSIONFILE
-elif [ "$1" = "clean" ]; then
-	echo Cleaning
-	rm -rf build
+echo Building $APP
+if [ -e "$VERSIONFILE" ]; then
+    rm $VERSIONFILE
 fi
+echo "package cmd" > $VERSIONFILE
+echo "const (" >> $VERSIONFILE
+echo "  VERSION = \"$VERSION\"" >> $VERSIONFILE
+echo "  BUILD_DATE = \"$BUILD_DATE\"" >> $VERSIONFILE
+echo ")" >> $VERSIONFILE
 
+glide install
+
+echo Building darwin-amd64
+GOOS=darwin GOARCH=amd64 go build -o build/bin/$APP-darwin-amd64
+echo Building linux-amd64
+GOOS=linux GOARCH=amd64 go build -o build/bin/$APP-linux-amd64
 
 
