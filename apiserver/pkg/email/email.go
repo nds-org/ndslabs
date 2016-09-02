6bdbf8698026235b487aaad75bc459668c339ac4
Craig Willis <willis8@illinois.edu>
2016-09-02 16:39:55 -0500

    NDS-467: Account approval changes (#119)
    * Added post email verification message indicating duration of view.
    
    * Updated message
    
    * Fixed typo
    
    * Fixed anonymous field name

--- apiserver/pkg/email/email.go
+++ apiserver/pkg/email/email.go
@@ -54,6 +54,29 @@ func (s *EmailHelper) SendVerificationEmail(name string, address string, url str
 	return nil
 }
 
+// Send email address verified
+func (s *EmailHelper) SendVerifiedEmail(name string, address string) error {
+
+	data := struct {
+		Name         string
+		SupportEmail string
+	}{
+		Name:         name,
+		SupportEmail: s.SupportEmail,
+	}
+
+	subject := "Registration pending"
+	msg, err := s.parseTemplate("templates/verified-email.html", data)
+	if err != nil {
+		return err
+	}
+	_, err = s.sendEmail(address, subject, msg)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
 // Send new account request email
 func (s *EmailHelper) SendNewAccountEmail(account *api.Account, approveUrl string, denyUrl string) error {
 
@@ -123,17 +146,19 @@ func (s *EmailHelper) SendStatusEmail(name string, address string, url string, a
 }
 
 // Send password recovery email
-func (s *EmailHelper) SendRecoveryEmail(name string, email string, recoveryUrl string) error {
+func (s *EmailHelper) SendRecoveryEmail(name string, email string, recoveryUrl string, unapproved bool) error {
 
 	data := struct {
 		Name         string
 		Email        string
 		Link         string
 		SupportEmail string
+		Unapproved   bool
 	}{
 		Name:         name,
 		Email:        email,
 		Link:         recoveryUrl,
+		Unapproved:   unapproved,
 		SupportEmail: s.SupportEmail,
 	}
 
@@ -142,7 +167,7 @@ func (s *EmailHelper) SendRecoveryEmail(name string, email string, recoveryUrl s
 	if err != nil {
 		return err
 	}
-	_, err = s.sendEmail(s.SupportEmail, subject, msg)
+	_, err = s.sendEmail(email, subject, msg)
 	if err != nil {
 		return err
 	}
