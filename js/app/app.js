43a57d1c1c3f4b0977309c94367dd56acc43120a
Craig Willis <willis8@illinois.edu>
2016-09-10 17:52:57 -0500

    Updated version to 1.0.4

--- js/app/app.js
+++ js/app/app.js
@@ -87,7 +87,7 @@ angular.module('ndslabs', [ 'navbar', 'footer', 'ndslabs-services', 'ndslabs-fil
 /**
  * The version/revision of this GUI
  */
-.constant('BuildVersion', '1.0.3-devel')
+.constant('BuildVersion', '1.0.4-devel')
 .constant('BuildDate', '')
 
 /**
