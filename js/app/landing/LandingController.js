01b85bffa2628a70712220a48602d5d082f68f89
Mike Lambert <lambert8@illinois.edu>
2016-09-23 21:38:23 +0000

    Merged LandingPage and VerifyAccount views into one

--- js/app/landing/LandingController.js
+++ js/app/landing/LandingController.js
@@ -8,11 +8,27 @@ angular
  * @author lambert8
  * @see https://opensource.ncsa.illinois.edu/confluence/display/~lambert8/3.%29+Controllers%2C+Scopes%2C+and+Partial+Views
  */
-.controller('LandingController', [ '$scope', '$location', '$log', '_', 'AuthInfo', 'OrgName', 'ProductName', 'ProductUrl', 'HelpLinks',  
-    function($scope, $location, $log, _, authInfo, OrgName, ProductName, ProductUrl, HelpLinks) {
+.controller('LandingController', [ '$scope', '$location', '$routeParams', '$log', '_', 'AuthInfo', 'OrgName', 'ProductName', 'NdsLabsApi', 'HelpLinks',  
+    function($scope, $location, $routeParams, $log, _, authInfo, OrgName, ProductName, NdsLabsApi, HelpLinks) {
   $scope.orgName = OrgName;
   $scope.productName = ProductName;
   $scope.helpLinks = HelpLinks;
   
   $scope.featureLink = _.find($scope.helpLinks, [ 'name', 'Feature Overview' ]);
+  
+  $scope.token = $routeParams.t;
+  $scope.user = $routeParams.u;
+  
+  $scope.productName = ProductName;
+  
+  if ($scope.user && $scope.token) {
+    $scope.verified = null;
+    NdsLabsApi.putRegisterVerify({ verify: { u: $scope.user, t: $scope.token } }).then(function(data) {
+      console.debug(data);
+      $scope.verified = true;
+    }, function(response) {
+      $log.error("Failed to verify user " + $scope.user + ":" + $scope.token);
+      $scope.verified = false;
+    });
+  }
 }]);
