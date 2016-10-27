4f82eda95200e3c678fa952dfe5a693f696453f4
Mike Lambert <lambert8@illinois.edu>
2016-10-27 00:27:42 -0500

    Sync to comment out extra browsers for now, until I get some specs written

--- conf.js
+++ conf.js
@@ -2,5 +2,27 @@
 exports.config = {
   framework: 'jasmine',
   seleniumAddress: 'http://localhost:4444/wd/hub',
+  chromeOnly: true,
+  capabilities: {
+	  'browsername': 'chrome'
+  },
+  
+  /*
+   * Specify the parameters of the browsers to test
+   */
+  /* multiCapabilities:[
+	{
+	  'browserName': 'firefox'
+	}, {
+	  'browserName': 'chrome'
+	},{
+	  'browserName': 'internet explorer',
+      'version': '11'
+	}
+  ],*/
+  
+  /*
+   * Specify which test spec(s) to run
+   */
   specs: ['tests/spec.js']
 }
