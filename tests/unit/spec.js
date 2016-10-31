ae315cedbf2493b471d289d3c75997ffcf6062fc
Mike Lambert <lambert8@illinois.edu>
2016-10-31 20:33:08 +0100

    Added simple unit tests to try and get karma working

---
+++ tests/unit/spec.js
@@ -0,0 +1,34 @@
+describe('SignUpController', function() {
+  beforeEach(module('ndslabs'));
+
+  var $controller;
+
+  beforeEach(inject(function(_$controller_){
+    // The injector unwraps the underscores (_) from around the parameter names when matching
+    $controller = _$controller_;
+  }));
+  describe('product name', function() {
+	it('has the correct value', function() {
+      var $scope = {};
+      var controller = $controller('SignUpController', { $scope: $scope });
+      expect($scope.productName).toEqual('Labs Workbench');
+	});
+  });
+  
+  describe('eula link', function() {
+	it('has the correct value', function (){
+      var $scope = {};
+      var controller = $controller('SignUpController', { $scope: $scope });
+      expect($scope.eulaLink).toEqual('https://nationaldataservice.atlassian.net/wiki/display/NDSC/Acceptable+Use+Policy');
+	});
+  });
+
+  describe('signUp form', function() {
+    it('is invalid if passwords do not match', function() {
+      var $scope = {};
+      var controller = $controller('SignUpController', { $scope: $scope });
+      $scope.newProject.password = 'longerthaneightchars';
+      expect($scope.registerForm.$valid).toEqual(false);
+    });
+  });
+});
