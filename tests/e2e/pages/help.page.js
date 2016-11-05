6d8bd3b67989d7d1c4b3fcf488e1755663042a72
Mike Lambert <lambert8@illinois.edu>
2016-11-05 17:17:45 +0100

    Filled out PageObject for the Contact Us view

--- tests/e2e/pages/help.page.js
+++ tests/e2e/pages/help.page.js
@@ -15,6 +15,22 @@ var TEST_HOSTNAME = shared.config.TEST_HOSTNAME;
 var PAGE_TITLE = 'Contact Labs Workbench Support';
 var PAGE_ROUTE = TEST_HOSTNAME + '/contact';
 
+var BTN_FORUM_ID = 'ggroupBtn';
+var BTN_CHAT_ID = 'gitterBtn';
+var BTN_EMAIL_ID = 'emailBtn';
+var COMBO_TYPE_ID = 'typeField';
+var CHECK_ANON_ID = 'anonymousInput';
+var BTN_SUBMIT_ID = 'feedbackBtn';
+
+var forumBtn = function() {  return element(by.id(BTN_FORUM_ID)); };
+var chatBtn = function() {  return element(by.id(BTN_CHAT_ID)); };
+var emailBtn = function() {  return element(by.id(BTN_EMAIL_ID)); };
+
+var feedbackTypeSelector = function() {  return element(by.id(COMBO_TYPE_ID)); };
+var feedbackAnonymousCheckbox = function() {  return element(by.id(CHECK_ANON_ID)); };
+
+var feedbackSubmitBtn = function() {  return element(by.id(BTN_SUBMIT_ID)); };
+
 // Ensure that we are on the correct page
 module.exports.verify = function() { 
   expect(browser.getCurrentUrl()).toBe(PAGE_ROUTE);
@@ -27,6 +43,26 @@ module.exports.get = function() {
   landing.clickContactUsLink();
   
   module.exports.verify();
+};
+
+// Click the forum button (i.e. Google Group)
+module.exports.clickForum = function() {
+  forumBtn().click();
+};
+
+// Click the chat button (i.e. Gitter, Slack, etc)
+module.exports.clickChat = function() {
+  chatBtn().click();
+};
+
+// Click the email button
+module.exports.toggleAnonymousCheckbox = function() {
+  emailBtn().click();
+};
+
+// Click (toggle) the anonymous feedback checkbox
+module.exports.clickEmail = function() {
+  feedbackAnonymousCheckbox().click();
 };
 
 // TODO: contact methods: gitter / group / e-mail
