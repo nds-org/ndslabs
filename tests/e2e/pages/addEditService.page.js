19d8066d24ff8fd3a4f8ff0635c9a8484b71ad39
Mike Lambert <lambert8@illinois.edu>
2016-11-12 08:24:43 +0000

    cleanup + filled out PageObject for AddEditServicePage

--- tests/e2e/pages/addEditService.page.js
+++ tests/e2e/pages/addEditService.page.js
@@ -16,9 +16,33 @@ var PAGE_TITLE = /Add Service/;
 var PAGE_ROUTE = /https\:\/\/.+\/\#\/home\/.+\/add\/.+/;
 
 var AddServicePage = function() {
-  /**
-   * Insert public Getters here for page elements
-   */
+  this.cfgBadge = element(by.id('cfgBadge'));
+  this.cfgNameInput = element(by.id('cfgName'));
+  this.newCfgValueType = element(by.id('newCfgValueType'));
+  this.cfgValueInput = element(by.id('cfgValue'));
+  this.cfgPasswordInput = element(by.id('cfgPassword'));
+  this.cfgPasswordStrength = element(by.id('cfgPasswordStrength'));
+  this.cfgDefaultBtn = element(by.id('cfgDefaultBtn'));
+  this.cfgRemoveBtn = element(by.id('cfgRemoveBtn'));
+  this.newCfgNameInput = element(by.id('newCfgName'));
+  this.newCfgValueInput = element(by.id('newCfgValue'));
+  this.newCfgValidationText = element(by.id('newCfgValidationText'));
+  this.cfgAddBtn = element(by.id('cfgAddBtn'));
+  
+  this.volumeBadge = element(by.id('volumeBadge'));
+  this.volumeFromInput = element(by.id('volumeFromInput'));
+  this.volumeToInput = element(by.id('volumeToInput'));
+  this.volumeRemoveBtn = element(by.id('volumeRemoveBtn'));
+  this.newVolumeFrom = element(by.id('newVolumeFrom'));
+  this.newVolumeTo = element(by.id('newVolumeTo'));
+  this.volumeAddBtn = element(by.id('volumeAddBtn'));
+  
+  this.dockerImageRegistryText = element(by.id('dockerImageRegistryText'));
+  this.dockerImageNameText = element(by.id('dockerImageNameText'));
+  this.dockerImageTagSelect = element(by.id('dockerImageTagSelect'));
+  
+  this.saveBtn = element(by.id('saveBtn'));
+  this.cancelBtn = element(by.id('cancelBtn'));
 };
 
 // Navigate to the App Service view
