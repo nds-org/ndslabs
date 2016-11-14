8ff70cb054402e38f0fdd4747babed8f0c5eab12
Mike Lambert <lambert8@illinois.edu>
2016-11-14 22:24:58 +0000

    Basic dashboard test suite (still missing a bit, but primary functionality is covered imo)

--- gui/tests/e2e/dashboard.e2e.js
+++ gui/tests/e2e/dashboard.e2e.js
@@ -11,7 +11,17 @@ var LandingPage = require('./pages/landing.page.js');
 var DashboardPage = require('./pages/dashboard.page.js');
 var CatalogPage = require('./pages/catalog.page.js');
 
+var AddOrEditServicePage = require('./pages/addEditService.page.js');
+
 var EC = protractor.ExpectedConditions;
+
+var TEXT_BASIC_AUTH_ENABLED = 'HTTP Basic Authentication Enabled';
+var TEXT_BASIC_AUTH_DISABLED = 'HTTP Basic Authentication Disabled';
+  
+var TEST_NEW_APPLICATION_NAME = 'New Label';
+
+var WAIT_TIME_APPLICATION_STARTUP = 120000;
+var WAIT_TIME_APPLICATION_SHUTDOWN = 120000;
     
 // dashboard.e2e.js
 describe('Labs Workbench Dashboard View', function() {
@@ -19,24 +29,14 @@ describe('Labs Workbench Dashboard View', function() {
   var landingPage = new LandingPage();
   var catalogPage = new CatalogPage();
   var dashboardPage = new DashboardPage();
-  
-  var WAIT_TIME_APPLICATION_SHUTDOWN = 120000;
+  var addServicePage = new AddOrEditServicePage();
+  var editServicePage = new AddOrEditServicePage();
   
   beforeAll(function(done) {
     helpers.beforeAll();
     dashboardPage.get();
     
-    // TODO: Shut down all applications
-    //console.log(dashboardPage.applications);
-    
-    var shutdownQueue = [];
-    var removeQueue = [];
-    
-    var expandCount = 0;
-    var stopCount = 0;
-    var removeCount = 0;
-    
-    // FIXME: Shutdown and remove all applications
+    // Shutdown and remove all applications
     dashboardPage.applications.then(function(applications) {
       for (let i = 0; i < applications.length; i++) {
         let application = applications[i];
@@ -91,11 +91,7 @@ describe('Labs Workbench Dashboard View', function() {
   });
 
   describe('With Applications', function() {
-    beforeAll(function() {  
-      // Install an application
-      catalogPage.get(true);
-    
-      var target = 'toolmanager';
+    var installApplication = function(target) {
       helpers.selectByModel(catalogPage.cards, "spec.key", function(key) { 
         return key === target; // How to know we've found our match
       }, 
@@ -103,95 +99,278 @@ describe('Labs Workbench Dashboard View', function() {
         catalogPage.addBtn(card).click();
         dashboardPage.get(true);
       });
-    });
+    };
+    
+    beforeAll(function() {  
+      // Install an application
+      catalogPage.get(true);
+      installApplication('toolmanager');
+    }, 120000);
     
     afterAll(function(done) { 
       // Remove the application
       dashboardPage.applications.each(function(application) {
-        removeOne(application);
+        dashboardPage.removeApplication(application);
         done();
       })
-    });
-  
-    // How to set up for test? Is it safe to simply delete all existing applications?
-    it('should enumerate added applications', function() {
-      expect(dashboardPage.applications.count()).toBe(1);
-    });
-    /*
+    }, 120000);
+    
     it('should allow the user to change the label of an application', function() {
-      
+      dashboardPage.applications.each(function(application) {
+        dashboardPage.applicationLabel(application).getText().then(function(oldLabel) {
+          // Rename an application
+          browser.wait(EC.elementToBeClickable(dashboardPage.renameBtn(application)), 5000);
+          dashboardPage.renameBtn(application).click();
+          
+          // Expect previous label to be the default value
+          browser.wait(EC.visibilityOf(dashboardPage.stackRenameModal), 5000);
+          browser.wait(EC.visibilityOf(dashboardPage.nameInput), 5000);
+          expect(dashboardPage.nameInput.getAttribute('value')).toBe(oldLabel);
+          
+          // Input new application label
+          dashboardPage.nameInput.clear();
+          dashboardPage.nameInput.sendKeys(TEST_NEW_APPLICATION_NAME);
+          dashboardPage.confirmBtn.click();
+          
+          // Ensure that the name changed as expected
+          browser.wait(EC.textToBePresentInElement(dashboardPage.applicationLabel(application), TEST_NEW_APPLICATION_NAME), 5000);
+          expect(dashboardPage.applicationLabel(application).getText()).toBe(TEST_NEW_APPLICATION_NAME);
+        
+          // Revert label back to reset test state
+          browser.wait(EC.elementToBeClickable(dashboardPage.renameBtn(application)), 5000);
+          dashboardPage.renameBtn(application).click();
+          browser.wait(EC.visibilityOf(dashboardPage.stackRenameModal), 5000);
+          browser.wait(EC.visibilityOf(dashboardPage.nameInput), 5000);
+          dashboardPage.nameInput.clear();
+          dashboardPage.nameInput.sendKeys(oldLabel);
+          dashboardPage.confirmBtn.click();
+          
+          // Ensure that the name reverted as expected
+          browser.wait(EC.textToBePresentInElement(dashboardPage.applicationLabel(application), oldLabel), 5000);
+          expect(dashboardPage.applicationLabel(application).getText()).toBe(oldLabel);
+        });
+      });
     });
     
     it('should allow basic auth toggle on select system applications', function() {
-      
+      dashboardPage.applications.each(function(application) {
+        // Expand application header
+        application.click();
+        browser.wait(EC.textToBePresentInElement(dashboardPage.toggleAuthBtn(application), 'Disabled'), 5000);
+        expect(dashboardPage.toggleAuthBtn(application).getText()).toBe(TEXT_BASIC_AUTH_DISABLED);
+        
+        // Enable basic auth
+        dashboardPage.toggleAuthBtn(application).click();
+        browser.wait(EC.textToBePresentInElement(dashboardPage.toggleAuthBtn(application), 'Enabled'), 5000);
+        expect(dashboardPage.toggleAuthBtn(application).getText()).toBe(TEXT_BASIC_AUTH_ENABLED);
+        
+        // Disable auth again to reset test state
+        dashboardPage.toggleAuthBtn(application).click();
+        browser.wait(EC.textToBePresentInElement(dashboardPage.toggleAuthBtn(application), 'Disabled'), 5000);
+        expect(dashboardPage.toggleAuthBtn(application).getText()).toBe(TEXT_BASIC_AUTH_DISABLED);
+      });
     });
     
     it('should prevent basic auth toggle on user applications', function() {
-      
+      // TODO
     });
     
     it('should prevent basic auth toggle on authRequired applications', function() {
-      
+      // TODO
     });
     
-    it('should link to the application\'s help documentation', function() {
-      
+    it('should link to each application service\'s help documentation', function() {
+      dashboardPage.applications.each(function(application) {
+        application.click();
+        dashboardPage.services(application).each(function(service) {
+          expect(dashboardPage.helpLink(service).isPresent()).toBe(true);
+          
+          // TODO: Verify destination
+        });
+      });
     });
     
     it('should allow the user to edit an application service', function() {
-      
+      dashboardPage.applications.each(function(application) {
+        application.click();
+        dashboardPage.services(application).each(function(service) {
+          dashboardPage.editServiceBtn(service).click();
+          editServicePage.verify();
+        });
+      });
     });
     
     it('should allow the user to add optional services to the application', function() {
-      
-    });
-    
-    // FIXME: Test order should not affect success
-    it('should allow the user to launch the application', function() {
-      
+      // TODO
     });
   
     // After starting an application
-    describe('Running', function() {
-      beforeAll(function() {  
+    describe('Running', function() {      
+      beforeAll(function(done) {  
         // Start the Application
-      });
-      afterAll(function() { 
+        dashboardPage.applications.each(function(application) {
+          dashboardPage.launchApplication(application);
+          done();
+        });
+      }, 120000);
+      
+      afterAll(function(done) { 
         // Stop the application
-      });
+        dashboardPage.applications.each(function(application) {
+          dashboardPage.shutdownApplication(application);
+          done();
+        });
+      }, 120000);
       
       it('should link to available endpoints on the service', function() {
-        
+        dashboardPage.applications.each(function(application) {
+          application.click();
+          
+          dashboardPage.services(application).each(function(service) {
+            expect(dashboardPage.endpointLinks(service).count()).toBe(1);
+            
+            dashboardPage.serviceIdText(service).getText().then(function(serviceId) {
+              dashboardPage.endpointLinks(service).first().click();
+              helpers.expectNewTabOpen(new RegExp("https\:\/\/" + serviceId + "\..*"));
+          
+              // TODO: How to handle basic auth?
+            });
+          });
+        });
       });
-      
-      it('should allow the user to change the label of a running application', function() {
-        
+    
+      it('should allow the user to change the label of an application', function() {
+        dashboardPage.applications.each(function(application) {
+          dashboardPage.applicationLabel(application).getText().then(function(oldLabel) {
+            // Rename an application
+            browser.wait(EC.elementToBeClickable(dashboardPage.renameBtn(application)), 5000);
+            dashboardPage.renameBtn(application).click();
+            
+            // Expect previous label to be the default value
+            browser.wait(EC.visibilityOf(dashboardPage.stackRenameModal), 5000);
+            browser.wait(EC.visibilityOf(dashboardPage.nameInput), 5000);
+            expect(dashboardPage.nameInput.getAttribute('value')).toBe(oldLabel);
+            
+            // Input new application label
+            dashboardPage.nameInput.clear();
+            dashboardPage.nameInput.sendKeys(TEST_NEW_APPLICATION_NAME);
+            dashboardPage.confirmBtn.click();
+            
+            // Ensure that the name changed as expected
+            browser.wait(EC.textToBePresentInElement(dashboardPage.applicationLabel(application), TEST_NEW_APPLICATION_NAME), 5000);
+            expect(dashboardPage.applicationLabel(application).getText()).toBe(TEST_NEW_APPLICATION_NAME);
+          
+            // Revert label back to reset test state
+            browser.wait(EC.elementToBeClickable(dashboardPage.renameBtn(application)), 5000);
+            dashboardPage.renameBtn(application).click();
+            browser.wait(EC.visibilityOf(dashboardPage.stackRenameModal), 5000);
+            browser.wait(EC.visibilityOf(dashboardPage.nameInput), 5000);
+            dashboardPage.nameInput.clear();
+            dashboardPage.nameInput.sendKeys(oldLabel);
+            dashboardPage.confirmBtn.click();
+            
+            // Ensure that the name reverted as expected
+            browser.wait(EC.textToBePresentInElement(dashboardPage.applicationLabel(application), oldLabel), 5000);
+            expect(dashboardPage.applicationLabel(application).getText()).toBe(oldLabel);
+          });
+        });
       });
       
       it('should allow the user to view the console of a running application', function() {
-        
+        dashboardPage.applications.each(function(application) {
+          application.click();
+          dashboardPage.services(application).each(function(service) {
+            browser.wait(EC.elementToBeClickable(dashboardPage.consoleBtn(service)), WAIT_TIME_APPLICATION_STARTUP);
+            dashboardPage.consoleBtn(service).click();
+            //consolePage.verify();
+            helpers.expectNewTabOpen(/.*console.*/);
+            
+            // TODO: Verify basic console functionality
+          });
+        });
       });
       
       it('should allow the user to view the config of a running application', function() {
-        
+        dashboardPage.applications.each(function(application) {
+          application.click();
+          dashboardPage.services(application).each(function(service) {
+            browser.wait(EC.elementToBeClickable(dashboardPage.viewConfigBtn(application)), 5000);
+            dashboardPage.viewConfigBtn(service).click();
+            
+            // Expect the viewConfigModal to pop up
+            expect(dashboardPage.viewConfigModal.isPresent()).toBe(true);
+            expect(dashboardPage.viewConfigModal.isDisplayed()).toBe(true);
+            
+            // Dismiss the modal
+            dashboardPage.cancelBtn.click();
+          });
+        });
       });
       
       it('should allow the user to view logs of a running application', function() {
-        
+        dashboardPage.applications.each(function(application) {
+          application.click();
+          dashboardPage.services(application).each(function(service) {
+            browser.wait(EC.elementToBeClickable(dashboardPage.viewLogsBtn(application)), 5000);
+            dashboardPage.viewLogsBtn(service).click();
+            
+            // Expect the viewLogsModal to pop up
+            expect(dashboardPage.viewLogsModal.isPresent()).toBe(true);
+            expect(dashboardPage.viewLogsModal.isDisplayed()).toBe(true);
+            
+            // Dismiss the modal
+            dashboardPage.cancelBtn.click();
+          });
+        });
       });
     
       it('should link to the application\'s help documentation', function() {
-        
+        dashboardPage.applications.each(function(application) {
+          application.click();
+          dashboardPage.services(application).each(function(service) {
+            expect(dashboardPage.helpLink(service).isPresent()).toBe(true);
+            
+            // TODO: Verify destination
+          });
+        });
       });
       
       it('should allow the user to shutdown a running application', function() {
-        
-      });
+        dashboardPage.applications.each(function(application) {
+          dashboardPage.shutdownApplication(application);
+          
+          browser.wait(EC.elementToBeClickable(dashboardPage.launchBtn(application)), WAIT_TIME_APPLICATION_STARTUP);
+          dashboardPage.launchApplication(application);
+          
+          browser.wait(EC.elementToBeClickable(dashboardPage.shutdownBtn(application)), WAIT_TIME_APPLICATION_STARTUP);
+        });
+      }, 300000);
     });
     
-    // FIXME: Test order should not affect success
-    it('should allow the user to remove the application', function() {
-      
+   /*it('should allow the user to add optional services to their application', function() {
+      // TODO
+    });
+    
+    it('should allow the user to remove optional services from their application', function() {
+      // TODO
     });*/
+    
+    // Test order should not affect success
+    it('should allow the user to remove the application', function() {
+      dashboardPage.applications.each(function(application) {
+        application.click();
+        
+        browser.wait(EC.elementToBeClickable(dashboardPage.deleteBtn(application)), WAIT_TIME_APPLICATION_STARTUP);
+        dashboardPage.deleteBtn(application).click();
+        dashboardPage.confirmBtn.click();
+      });
+      
+      // Reinstall the application to reset test state
+      catalogPage.get(true);
+      installApplication('toolmanager');
+      
+      
+      dashboardPage.get(true);
+    }, 120000);
   });
 });
--- gui/tests/e2e/pages/dashboard.page.js
+++ gui/tests/e2e/pages/dashboard.page.js
@@ -21,30 +21,46 @@ var DashboardPage = function() {
   this.helperText = element(by.id('dashHelperText'));
   this.catalogLink = element(by.id('catalogLink'));
   
+  // Repeaters
   this.applications = element.all(by.repeater("stack in configuredStacks | orderBy:['name','id'] track by stack.id"));
-  this.services = function(app) {  return app.element.all(by.repeater("svc in stack.services track by svc.id")); };
+  this.services = function(app) {  return app.all(by.repeater("svc in stack.services track by svc.id")); };
+  this.endpointLinks = function(svc) {  return svc.all(by.repeater('endpt in svc.endpoints track by endpt.port')); };
+  
+  // Modal (popup) windows
+  this.viewConfigModal = element(by.id('viewConfigModal'));
+  this.viewLogsModal = element(by.id('viewLogsModal'));
+  this.stackDeleteModal = element(by.id('stackDeleteModal'));
+  this.stackRenameModal = element(by.id('stackRenameModal'));
+  this.stackStopModal = element(by.id('stackStopModal'));
   
   // Shared Modal Stuff
-  this.nameInput = element(by.id('nameInput'));       // Rename modal
   this.confirmBtn = element(by.id('confirmBtn'));
   this.cancelBtn = element(by.id('cancelBtn'));
   this.closeBtn = element(by.id('closeBtn'));
   
-
+  // Other Modal Stuff
+  this.nameInput = element(by.id('nameInput'));       // Rename modal
+  this.logBody = element(by.id('logBody'));           // View logs modal
+  
   // Application Stuff
+  this.applicationLabel = function(app) {  return app.element(by.id('applicationLabel')); };
   this.renameBtn = function(app) {  return app.element(by.id('renameBtn')); };
   this.toggleAuthBtn = function(app) {  return app.element(by.id('toggleAuthBtn')); };
   this.launchBtn = function(app) {  return app.element(by.id('launchBtn')); };
   this.deleteBtn = function(app) {  return app.element(by.id('deleteBtn')); };
+  
   this.addServiceBtn = function(app) {  return app.element(by.id('addServiceBtn')); };
+  
   this.shutdownBtn = function(app) {  return app.element(by.id('shutdownBtn')); };
 
   // Service Stuff
+  this.serviceIdText = function(svc) {  return svc.element(by.id('serviceIdText')); };
   this.statusText = function(svc) {  return svc.element(by.id('statusText')); };
   this.editServiceBtn = function(svc) {  return svc.element(by.id('editServiceBtn')); };
   this.helpLink = function(svc) {  return svc.element(by.id('helpLink')); };
+  
   this.removeServiceBtn = function(svc) {  return svc.element(by.id('removeBtn')); };
-  this.endpointLink = function(svc) {  return svc.element(by.id('endpointLink')); };
+  
   this.consoleBtn = function(svc) {  return svc.element(by.id('consoleBtn')); };
   this.viewLogsBtn = function(svc) {  return svc.element(by.id('viewLogsBtn')); };
   this.viewConfigBtn = function(svc) {  return svc.element(by.id('viewConfigBtn')); };
@@ -75,36 +91,49 @@ DashboardPage.prototype.verify = function() {
   expect(browser.getTitle()).toEqual(PAGE_TITLE);
 };
 
+DashboardPage.prototype.launchApplication = function(application) {
+  var launchBtn = this.launchBtn(application);
+  //if (!launchBtn.isPresent()) {
+    application.click();
+  //}
+  
+  // Wait for the shutdown button to be clickable
+  browser.wait(EC.elementToBeClickable(launchBtn), 120000);
+  launchBtn.click();
+  
+  browser.wait(function() {
+    return helpers.hasClass(application, 'panel-success');
+  }, 120000);
+  expect(helpers.hasClass(application, 'panel-success')).toBe(true); 
+};
+
 DashboardPage.prototype.shutdownApplication = function(application) {
+  var shutdownBtn = this.shutdownBtn(application);
   //if (!shutdownBtn.isPresent()) {
     application.click();
   //}
   
-  var shutdownBtn = this.shutdownBtn(application);
-  if (shutdownBtn.isPresent()) {
-    shutdownBtn.click();
-    this.confirmBtn.click();
-  } else {
-    console.log("ERROR: Shutdown button cannot be clicked: not present");
-  }
+  // Wait for the shutdown button to be clickable
+  browser.wait(EC.elementToBeClickable(shutdownBtn), 120000);
+  shutdownBtn.click();
+  this.confirmBtn.click();
+  
+  // Wait for the application to shut down before returning
+  browser.wait(function() {
+    return helpers.hasClass(application, 'panel-danger');
+  }, 120000);
+  expect(helpers.hasClass(application, 'panel-danger')).toBe(true); 
 };
 
 DashboardPage.prototype.removeApplication = function(application) {
+  var deleteBtn = this.deleteBtn(application);
   //if (!deleteBtn.isPresent()) {
     application.click();
   //}
   
-  var deleteBtn = this.deleteBtn(application);
-  if (deleteBtn.isPresent()) {
-    deleteBtn.click();
-    this.confirmBtn.click();
-  } else {
-    console.log("ERROR: Delete button cannot be clicked: not present");
-  }
-  /*return helpers.selectByModel(this.applications, "stack.id", function(application) { 
-    return application.id === application; // How to know we've found our match
-  }, predicate);*/
-  
+  browser.wait(EC.elementToBeClickable(deleteBtn), 120000);
+  deleteBtn.click();
+  this.confirmBtn.click();
 };
 
 module.exports = DashboardPage;
