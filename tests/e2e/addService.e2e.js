a43882bc4a0f6e178ef1ff8254e96dc2c854a320
Mike Lambert <lambert8@illinois.edu>
2016-11-18 13:47:36 -0600

    Fixed AddService test specs

--- tests/e2e/addService.e2e.js
+++ tests/e2e/addService.e2e.js
@@ -9,11 +9,16 @@ var Navbar = require('./pages/navbar.page.js');
 var DashboardPage = require('./pages/dashboard.page.js');
 var LandingPage = require('./pages/landing.page.js');
 var CatalogPage = require('./pages/catalog.page.js');
-var ConsolePage = require('./pages/catalog.page.js');
 var AddEditServicePage = require('./pages/addEditService.page.js');
 
 var TEST_SPEC_KEY = 'clowder';
 var TEST_SERVICE_INDEX_TO_ADD = 0;
+
+var TEST_CFG_NAME = 'ENV_VAR_NAME';
+var TEST_CFG_VALUE = 'some long env var that can be used for e2e testing purposes';
+
+var TEST_VOLUME_FROM = 'AppData';
+var TEST_VOLUME_TO = '/appdata';
 
 var EC = protractor.ExpectedConditions;
 
@@ -23,7 +28,6 @@ describe('Labs Workbench Add Optional Application Service View', function() {
   var landingPage = new LandingPage();
   var dashboardPage = new DashboardPage();
   var catalogPage = new CatalogPage();
-  var consolePage = new ConsolePage();
   var addServicePage = new AddEditServicePage();
 
   var stackId;
@@ -135,6 +139,145 @@ describe('Labs Workbench Add Optional Application Service View', function() {
       // Remove this service to reset the test state
       var removeServiceBtn = dashboardPage.removeServiceBtn(service);
       removeServiceBtn.click();
+    });
+  });
+  
+  describe('Environment Tab', function() {
+    beforeEach(function() {
+      addServicePage.environmentTab.click();
+      
+      // Verify initial page state
+      expect(addServicePage.cfgAddBtn.isPresent()).toBe(true);
+      expect(addServicePage.cfgAddBtn.isDisplayed()).toBe(true);
+      expect(addServicePage.cfgAddBtn.isEnabled()).toBe(false);
+      
+      expect(addServicePage.newCfgNameInvalidText.isPresent()).toBe(true);
+      expect(addServicePage.newCfgNameInvalidText.getText()).toContain('New variable requires a name!');
+      
+      expect(addServicePage.newCfgValueInvalidText.isPresent()).toBe(true);
+      expect(addServicePage.newCfgValueInvalidText.getText()).toContain('New variable requires a value!');
+      
+    });
+    
+    it('should allow the user to add custom environment variables', function() {
+      // Verify initial page state
+      expect(addServicePage.cfgAddBtn.isEnabled()).toBe(false);
+      // Enter a variable name
+      addServicePage.newCfgNameInput.sendKeys(TEST_CFG_NAME);
+      expect(addServicePage.newCfgNameInvalidText.isPresent()).toBe(false);
+      
+      // Enter a variable value
+      addServicePage.newCfgValueInput.sendKeys(TEST_CFG_VALUE);
+      expect(addServicePage.newCfgValueInvalidText.isPresent()).toBe(false);
+      
+      // Ensure that we can now add the config
+      expect(addServicePage.newCfgValidText.getText()).toContain('New variable is valid!');
+      expect(addServicePage.cfgAddBtn.isEnabled()).toBe(true);
+      addServicePage.cfgAddBtn.click();
+    });
+    
+    describe('With Configs', function() {
+      beforeEach(function() {
+        addServicePage.addConfig(TEST_CFG_NAME, TEST_CFG_VALUE);
+      });
+      
+      it('should prevent the user from adding duplicate variables', function() {
+        // Verify initial page state
+        expect(addServicePage.newCfgNameInvalidText.getText()).toContain('New variable requires a name!');
+        expect(addServicePage.newCfgValueInvalidText.getText()).toContain('New variable requires a value!');
+                
+        // Enter a variable value
+        addServicePage.newCfgValueInput.sendKeys(TEST_CFG_VALUE);
+        expect(addServicePage.newCfgValueInvalidText.isPresent()).toBe(false);
+        
+        // Enter a variable name
+        addServicePage.newCfgNameInput.sendKeys(TEST_CFG_NAME);
+        expect(addServicePage.newCfgNameInvalidText.isPresent()).toBe(true);
+        expect(addServicePage.newCfgNameInvalidText.getText()).toContain('New variable name is not unique!');
+
+        // Ensure that we cannot add the config
+        expect(addServicePage.newCfgValidText.isPresent()).toBe(false);
+        expect(addServicePage.cfgAddBtn.isEnabled()).toBe(false);
+      });
+      
+      it('should allow the user to remove custom environment variables', function() {
+        addServicePage.configs.then(function(configs) {
+          for(let i = 0; i < configs.length; i++) {
+            let cfg = configs[i];
+            expect(addServicePage.cfgRemoveBtn(cfg).isPresent()).toBe(true);
+            expect(addServicePage.cfgRemoveBtn(cfg).isDisplayed()).toBe(true);
+            expect(addServicePage.cfgRemoveBtn(cfg).isEnabled()).toBe(true);
+            addServicePage.cfgRemoveBtn(cfg).click();
+          }
+        });
+      });
+    });
+  });
+  
+  describe('Data Tab', function() {
+    beforeEach(function() {
+      addServicePage.dataTab.click();
+      expect(addServicePage.volumeAddBtn.isPresent()).toBe(true);
+      expect(addServicePage.volumeAddBtn.isDisplayed()).toBe(true);
+    });
+    
+    it('should allow the user to add custom volume mounts', function() {
+      // Verify initial page state
+      expect(addServicePage.volumeAddBtn.isEnabled()).toBe(false);
+      
+      // Enter a mount source 
+      addServicePage.newVolumeFrom.sendKeys(TEST_VOLUME_FROM);
+      
+      // Enter a mount destnation
+      addServicePage.newVolumeTo.sendKeys(TEST_VOLUME_TO);
+      
+      // Ensure that we can now add the volume
+      expect(addServicePage.volumeAddBtn.isEnabled()).toBe(true);
+      addServicePage.volumeAddBtn.click();
+      addServicePage.saveBtn.click();
+    });
+    
+    describe('With Volumes', function() {
+      beforeEach(function() {
+        addServicePage.addVolume(TEST_VOLUME_FROM, TEST_VOLUME_TO);
+      });
+      
+      it('should prevent the user from adding duplicate volumes', function() {
+        // Verify initial page state
+        expect(addServicePage.volumeAddBtn.isEnabled()).toBe(false);
+        
+        // Enter a mount source 
+        addServicePage.newVolumeFrom.sendKeys(TEST_VOLUME_FROM);
+        
+        // Enter a mount destnation
+        addServicePage.newVolumeTo.sendKeys(TEST_VOLUME_TO);
+        
+        // Ensure that we cannot add the duplicate volume
+        expect(addServicePage.volumeAddBtn.isEnabled()).toBe(false);
+      });
+      
+      it('should allow the user to remove custom volumes', function() {
+        addServicePage.volumes.then(function(volumes) {
+          for(let i = 0; i < volumes.length; i++) {
+            let vol = volumes[i];
+            expect(addServicePage.volumeRemoveBtn(vol).isPresent()).toBe(true);
+            expect(addServicePage.volumeRemoveBtn(vol).isDisplayed()).toBe(true);
+            expect(addServicePage.volumeRemoveBtn(vol).isEnabled()).toBe(true);
+            addServicePage.volumeRemoveBtn(vol).click();
+          }
+        });
+      });
+    });
+  });
+  
+  describe('Docker Tab', function() {
+    beforeEach(function() {
+      addServicePage.dockerTab.click();
+    });
+    
+    it('should allow the user to change the docker tag of the service', function() {
+      expect(addServicePage.dockerImageTagSelect.isPresent()).toBe(true);
+      expect(addServicePage.dockerImageTagSelect.isDisplayed()).toBe(true);
     });
   });
 });
