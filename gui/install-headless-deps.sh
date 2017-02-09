2f2462297b44e87e5dc610f78dccc2fcbae7f7bb
Mike Lambert <lambert8@illinois.edu>
2017-02-09 10:40:07 -0600

    Update install-headless-deps.sh

--- gui/install-headless-deps.sh
+++ gui/install-headless-deps.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
-# Enable backports for Debian (Jessie)
-echo "deb http://http.debian.net/debian jessie-backports main" >> /etc/apt/sources.list
-
-# Install Java 8 + Xvfb + Chromium
+# Install Xvfb + Chromium
 apt-get -qq update && \
-apt-get -qq install \
-    openjdk-8-jre-headless \
-    openjdk-8-jdk \
+apt-get -qq install -t jessie-backports \
     chromium \
     xvfb
 
