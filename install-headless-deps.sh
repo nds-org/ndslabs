0e2b653766e200ea3285d51563da0ac70e382d2b
Mike Lambert <lambert8@illinois.edu>
2017-02-09 12:59:35 -0600

    Fix broken WebUI on master (#177)
    * Fixed entrypoint
    
    * Update Dockerfile
    
    * Update Dockerfile
    
    * Update install-headless-deps.sh
    
    * Update Gruntfile.js
    
    * Update entrypoint.sh
    
    * Update entrypoint.sh
    
    * Update Dockerfile.ng-base
    
    * Testing that protractor still works via Grunt
    
    * Sync

--- install-headless-deps.sh
+++ install-headless-deps.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
 
-# Enable backports for Debian (Jessie)
-echo "deb http://http.debian.net/debian jessie-backports main" >> /etc/apt/sources.list
+# Add Firefox debian repo and key
+echo 'Installing E2E dependencies...'
+echo 'deb http://mozilla.debian.net/ jessie-backports firefox-release' > /etc/apt/sources.list.d/debian-mozilla.list && \
+curl mozilla.debian.net/pkg-mozilla-archive-keyring_1.1_all.deb -o ~/pkg-mozilla-archive-keyring_1.1_all.deb && \
+dpkg -i ~/pkg-mozilla-archive-keyring_1.1_all.deb
 
-# Install Java 8 + Xvfb + Chromium
-apt-get -qq update && \
-apt-get -qq install \
+# Install Xvfb + Chromium + Firefox
+apt-get -y update && \
+apt-get -qq install --no-install-recommends -t jessie-backports \
     openjdk-8-jre-headless \
     openjdk-8-jdk \
+    xvfb \
+    libgconf-2-4 \
+    libexif12 \
+    firefox \
     chromium \
-    xvfb
+    supervisor \
+    netcat-traditional \
+    curl \
+    ffmpeg \
+    gnupg
 
 # Install protractor / webdriver-manager
-npm install -g protractor@5.0.x
+npm install -g protractor@4.x
