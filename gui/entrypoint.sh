b873d73750c359858817c48e1181a60877e29d00
Mike Lambert <lambert8@illinois.edu>
2017-02-24 23:13:49 +0000

    and again

--- gui/entrypoint.sh
+++ gui/entrypoint.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-echo "Sedding"
-
 # Substitute the APISERVER_HOST and PORT passed in by "docker run -e" or kubernetes
 /bin/sed -i -e "s#^\.constant('ApiHost', '.*')#.constant('ApiHost', '${APISERVER_HOST}')#" "$BASEDIR/app/app.js"
 /bin/sed -i -e "s#^\.constant('ApiPort', '.*')#.constant('ApiPort', '${APISERVER_PORT}')#" "$BASEDIR/app/app.js"
@@ -14,22 +12,18 @@ echo "Sedding"
 # Substitute the ANALYTICS_ACCOUNT passed in by "docker run -e" or kubernetes
 /bin/sed -i -e "s#^\.constant('GaAccount', .*)#.constant('GaAccount', '${ANALYTICS_ACCOUNT}')#" "$BASEDIR/app/app.js"
 
-echo "Installing dependencies"
-
 # Install dependencies and start ExpressJS
-cd $BASEDIR && \
 npm install && \
 bower install --allow-root --config.interactive=false
 
 echo "Dropping in"
 
 # Grab any drop-ins from envvars, if specified
-if [ "${GIT_DROPIN_REPO}" != "" ]; then
+if [ "${GIT_DROPIN_REPO}" == "" ]; then
     # Copy source over existing
     echo "cloning" && git clone --single-branch --depth=1 -b ${GIT_DROPIN_BRANCH:-master} "${GIT_DROPIN_REPO}" /tmp/dropin && echo "post-clone"
     echo "copying" && cp -r /tmp/dropin/gui/* $BASEDIR && echo "post-copy"
+    echo "Grunting" && grunt
 fi
 
-echo "Grunting"
-
-grunt
+echo "Grunting" && grunt
