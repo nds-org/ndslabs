66f02db59a1666d21fcdc8c148229f434f2bfb25
Mike Lambert <lambert8@illinois.edu>
2017-02-24 21:02:51 +0000

    Try to fix the built version of the image

--- gui/entrypoint.sh
+++ gui/entrypoint.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Grab any drop-ins from envvars, if specified
+if [ "${GIT_DROPIN_REPO}" != "" ]; then
+    # Copy source over existing
+    echo "Using drop-in: git clone -b ${GIT_DROPIN_BRANCH:-master} ${GIT_DROPIN_REPO}"
+    git clone -b ${GIT_DROPIN_BRANCH:-master} ${GIT_DROPIN_REPO} && cp -r ndslabs/gui/* /home/
+fi
+
 # Substitute the APISERVER_HOST and PORT passed in by "docker run -e" or kubernetes
 /bin/sed -i -e "s#^\.constant('ApiHost', '.*')#.constant('ApiHost', '${APISERVER_HOST}')#" "$BASEDIR/app/app.js"
 /bin/sed -i -e "s#^\.constant('ApiPort', '.*')#.constant('ApiPort', '${APISERVER_PORT}')#" "$BASEDIR/app/app.js"
@@ -12,16 +19,8 @@
 # Substitute the ANALYTICS_ACCOUNT passed in by "docker run -e" or kubernetes
 /bin/sed -i -e "s#^\.constant('GaAccount', .*)#.constant('GaAccount', '${ANALYTICS_ACCOUNT}')#" "$BASEDIR/app/app.js"
 
-# Grab any drop-ins from envvars, if specified
-if [ "${GIT_DROPIN_REPO}" != "" ]; then
-    # Copy source over existing
-    echo "Using drop-in: git clone -b ${GIT_DROPIN_BRANCH:-master} ${GIT_DROPIN_REPO}"
-    mkdir -p /tmp/dropin && cd /tmp/dropin && git clone -b ${GIT_DROPIN_BRANCH:-master} ${GIT_DROPIN_REPO} && cp -r ndslabs/gui/* /home/
-fi
-
-cd $BASEDIR
-
 # Install dependencies and start ExpressJS
+cd $BASEDIR && \
 npm install && \
 bower install --allow-root --config.interactive=false && \
 grunt
