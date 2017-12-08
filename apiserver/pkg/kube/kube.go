80cb21475953ac9067598e30ce4b65f05a917b84
Craig Willis <willis8@illinois.edu>
2017-12-08 12:30:14 -0600

    Fixed build error due to Stream call changes

--- apiserver/pkg/kube/kube.go
+++ apiserver/pkg/kube/kube.go
@@ -1155,7 +1155,12 @@ func (k *KubeHelper) Exec(pid string, pod string, container string, kube *KubeHe
 			}
 		}()
 
-		err = e.Stream(remotecommandserver.SupportedStreamingProtocols, inr, outw, nil, true)
+		err = e.Stream(remotecommand.StreamOptions{
+			SupportedProtocols: remotecommandserver.SupportedStreamingProtocols,
+			Stdin:              inr,
+			Stdout:             outw,
+			Tty:                true,
+		})
 		if err != nil {
 			glog.Error(err)
 			return
@@ -1560,7 +1565,13 @@ func (k *KubeHelper) ExecCommand(pid string, pod string, command []string) (stri
 
 	localOut := &bytes.Buffer{}
 	localErr := &bytes.Buffer{}
-	err = e.Stream(remotecommandserver.SupportedStreamingProtocols, nil, localOut, localErr, false)
+	err = e.Stream(remotecommand.StreamOptions{
+		SupportedProtocols: remotecommandserver.SupportedStreamingProtocols,
+		Stdin:              nil,
+		Stdout:             localOut,
+		Stderr:             localErr,
+		Tty:                true,
+	})
 	return localOut.String(), err
 }
 
