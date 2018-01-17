78d591d101bc50fe23e6bcf8bfe65ea8a0cc083f
Mike Lambert <lambert8@illinois.edu>
2018-01-17 13:05:39 -0600

    Adjusted wording for quickstart link buttons, add a framename for quickstart to prevent multiple tabs opening (#233)

--- shared/services.js
+++ shared/services.js
@@ -80,7 +80,8 @@ angular.module('ndslabs-services', [ 'ndslabs-api' ])
           var cc = _.find(stack.services, [ 'service', quickstartKey ]);
           var ep = _.head(cc.endpoints);
           if (ep) {
-            Popup.open($filter('externalHostPort')(ep));
+            var url = $filter('externalHostPort')(ep);
+            Popup.open(url, "quickstart");
           }
         },
         launch: function() {
