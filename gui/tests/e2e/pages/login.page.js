637b0570696a00e4e8d11759227bdbaaa8138611
Mike Lambert <lambert8@illinois.edu>
2018-01-18 17:10:11 +0000

    Fixed expected login route

--- gui/tests/e2e/pages/login.page.js
+++ gui/tests/e2e/pages/login.page.js
@@ -7,7 +7,7 @@ var Navbar = require('./navbar.page.js');
 var LandingPage = require('./landing.page.js');
 
 var PAGE_TITLE = 'Sign In to Labs Workbench';
-var PAGE_ROUTE = /\/\#\/login/;
+var PAGE_ROUTE = /https?\:\/\/.+\/login\/\#?\/?/;
 
 var LoginPage = function() {
   "use strict";
