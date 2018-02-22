0ae5634fcb3a2abb467f57540dc26bbe4588fe99
Mike Lambert <lambert8@illinois.edu>
2018-02-22 16:01:26 -0600

    NDS-1127 / NDS-229: Rework Docker/Grunt build process, migrate away from using Bower

--- gui/entrypoint.sh
+++ gui/entrypoint.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-# Grab any drop-ins from envvars, if specified
-if [ "${GIT_DROPIN_REPO}" != "" ]; then
-    echo "Using drop-in: git clone --single-branch --depth=1 -b ${GIT_DROPIN_BRANCH:-master} ${GIT_DROPIN_REPO} /tmp/dropin"
-
-    # Merge drop-in into existing $BASEDIR (exclude entrypoint.sh, so we don't mess up our current execution)
-    git clone --single-branch --depth=1 -b ${GIT_DROPIN_BRANCH:-master} ${GIT_DROPIN_REPO} /tmp/dropin && \
-    rm -f /tmp/dropin/gui/entrypoint.sh && \
-    cp -r /tmp/dropin/gui/* "$BASEDIR/"
-fi
-
 # Substitute the APISERVER_HOST and PORT passed in by "docker run -e" or kubernetes
 /bin/sed -i -e "s#^\.constant('ApiHost', '.*')#.constant('ApiHost', '${APISERVER_HOST}')#" "$BASEDIR/ConfigModule.js"
 /bin/sed -i -e "s#^\.constant('ApiPort', '.*')#.constant('ApiPort', '${APISERVER_PORT}')#" "$BASEDIR/ConfigModule.js"
@@ -23,7 +13,5 @@ fi
 # Substitute the ANALYTICS_ACCOUNT passed in by "docker run -e" or kubernetes
 /bin/sed -i -e "s#^\.constant('GaAccount', .*)#.constant('GaAccount', '${ANALYTICS_ACCOUNT}')#" "$BASEDIR/ConfigModule.js"
 
-# Install dependencies and start ExpressJS
-npm install && \
-bower install --allow-root --config.interactive=false && \
+# Start ExpressJS
 grunt
