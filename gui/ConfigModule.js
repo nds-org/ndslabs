09e1164aa33c7506d97e54c8e6b762b12d553e88
Craig Willis <willis8@illinois.edu>
2018-03-28 13:25:50 -0500

    ischool custom landing page

--- gui/ConfigModule.js
+++ gui/ConfigModule.js
@@ -48,18 +48,16 @@ angular
 /**
  * The name of the product to display in the UI and the URL to link to when clicked
  */
-.constant('OrgName', 'NDS')
-.constant('ProductName', 'Labs Workbench')
-.constant('ProductUrl', 'http://www.nationaldataservice.org/projects/labs.html')
+.constant('OrgName', 'iSchool')
+.constant('ProductName', 'Data Curation Workbench')
+.constant('ProductUrl', 'https://nationaldataservice.atlassian.net/wiki/display/NDSC/Data+Curation+Workbench')
 
 /**
  * Helpful links to include in the landing page / navbar
  */
 .value('HelpLinks', [
   { name: "Feature Overview",       icon: 'fa-info-circle',        url: 'https://nationaldataservice.atlassian.net/wiki/display/NDSC/Feature+Overview' },
-  { name: "F.A.Q.",                 icon: 'fa-question-circle',    url: 'https://nationaldataservice.atlassian.net/wiki/display/NDSC/Frequently+Asked+Questions'},
   { name: "User's Guide",           icon: 'fa-book',               url: 'https://nationaldataservice.atlassian.net/wiki/display/NDSC/User%27s+Guide' },
-  { name: "Developer's Guide",      icon: 'fa-code-fork',          url: 'https://nationaldataservice.atlassian.net/wiki/display/NDSC/Developer%27s+Guide' },
   { name: "Acceptable Use Policy",  icon: 'fa-gavel',              url: 'https://nationaldataservice.atlassian.net/wiki/display/NDSC/Acceptable+Use+Policy' },
 ])
 
