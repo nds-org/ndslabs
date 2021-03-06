ebee9d9acca150b6248fd398afc4fdf6350978d6
Mike Lambert <lambert8@illinois.edu>
2021-03-05 18:42:23 -0600

    Fix hard-coded path

--- docs/serve.sh
+++ docs/serve.sh
@@ -5,7 +5,7 @@ PORT=${1:-8080}
 BUILD_DIR="./_build/html"
 
 if [ -d "$BUILD_DIR" ]; then
-	docker run -it --rm -v $(pwd)/_build/html:/usr/share/nginx/html -p ${PORT}:80 nginx
+	docker run -it --rm -v $(pwd)/${BUILD_DIR}:/usr/share/nginx/html -p ${PORT}:80 nginx
 else
 	echo 'No built HTML detected. Please run "./build.sh" first.'	
 fi
