93d81c95996849681057330f196336184174c9b5
Dukyun Nam <dynam@kisti.re.kr>
2021-04-15 18:24:49 -0500

    Retrieve services from etcd

--- api/services.py
+++ api/services.py
@@ -3,26 +3,24 @@ import logging
 import pkg.config as config
 from pkg.etcd import WBEtcd
 
-#import json
+import json
 import connexion
 
 
 # go version - GetAllServices
 def search():
-    etcdClient = WBEtcd(port=config.ETCD_PORT)
+    etcdClient = WBEtcd(host=config.ETCD_HOST, port=config.ETCD_PORT)
     etcdBasePath = config.ETCD_BASE_PATH
 
     args = connexion.request.args
-    logging.debug(args)
     catalog = args['catalog']
+    services = []
 
     if catalog == 'system':
-        services = etcdClient.getGlobalServices(args)
-    # elif args['catalog'] == 'user':
-    #    key = '/ndslabs/services/'
-    else:
-        services = etcdClient.getGlobalServices(args)
+        services = etcdClient.getSystemServices()
+    elif catalog == 'user':
+        services = etcdClient.getUserServices()
+    else:  # catalog == all or others
+        services = etcdClient.getAllServices()
 
-    #result = etcdClient.read('/ndslabs/services/')
-    #result = etcdClient.read(key)
-    return services._children
+    return services
--- pkg/etcd.py
+++ pkg/etcd.py
@@ -1,12 +1,14 @@
 import logging
 
 import etcd
+import json
+
 import pkg.config as config
 
 
 class WBEtcd:
-    def __init__(self, port=4001):
-        self.client = etcd.Client(port=port)
+    def __init__(self, host='127.0.0.1', port=4001):
+        self.client = etcd.Client(host=host, port=port)
         # wont let you run sensitive commands on non-leader machines, default is true
         #client = etcd.Client(host='127.0.0.1', port=4003, allow_redirect=False)
         # client = etcd.Client(
@@ -15,13 +17,40 @@ class WBEtcd:
         #    allow_reconnect=True,
         #    protocol='https',)
 
-    def read(self, key):
-        print(key)
-        return self.client.read(key)
+    def getSystemServices(self):
+        def get_index(items):
+            return items.get('modifiedIndex')
 
-    def getGlobalServices(self, args):
-        logging.debug("getGlobalService - catalog: "+args['catalog'])
         key = config.ETCD_BASE_PATH+"/services"
-        services = self.client.read(key)
+        results = self.client.read(key)
+        services = []
+
+        for item in sorted(results._children, key=get_index):
+            json_item = json.loads(item['value'])
+            json_item['catalog'] = 'system'
+            services.append(json_item)
+
+        return services
+
+    def getUserServices(self):
+        def get_index(items):
+            return items.get('modifiedIndex')
+
+        # need to get uid
+        uid = 'temp_id'
+        key = config.ETCD_BASE_PATH+"/accounts/"+uid+"/services",
+        results = self.client.read(key)
+        services = []
+
+        for item in sorted(results._children, key=get_index):
+            json_item = json.loads(item['value'])
+            json_item['catalog'] = 'user'
+            services.append(json_item)
+
+        return services
+
+    def getAllServices(self):
+        services = self.getSystemServices()
+        # services.append(self.getUserServices())
 
         return services
